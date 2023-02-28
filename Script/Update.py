95c79f64b3c091e2841e97f4a71a65e29e3f7d02
Rokate2023 <77772464+Rokate@users.noreply.github.com>
2023-02-28 16:54:27 +0800

    Update Update.py

--- Script/Update.py
+++ Script/Update.py
@@ -1,3 +1,10 @@
 from pathlib import Path
 
-print(str(Path.cwd()))
+all_file = []
+files = Path('./aomen').rglob('*.*')
+
+for file in files:
+    if Path.is_file(file):
+        all_file.append(file)
+
+print(len(all_file))
