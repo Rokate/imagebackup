deb87e1163c6582d468147f71ed3cad535b14722
Rokate2023 <77772464+Rokate@users.noreply.github.com>
2023-03-01 18:27:47 +0800

    Update Download.py

--- Script/Download.py
+++ Script/Download.py
@@ -1,8 +1,10 @@
 import requests
+import traceback
 
-picurl = requests.get('https://49152c.com/unite49/h5/picture/detail/latest?pictureTypeId=10870').json()['data']['largePictureUrl']
-pic02 = requests.get(picurl)
-if pic02.headers['Content-Type'] == 'image/jpeg':
-    print ('图下02')
-    with open('./aomen/11111111111.jpg', 'wb') as f:
-        f.write(pic02.content)
+try:
+    respic = requests.get('https://49152c.com/unite49/h5/picture/detail/latest?pictureTypeId=1087055').json()['data']['largePictureUrl']
+
+except Exception as e:
+    print('出错了',traceback.format_exc())
+else:
+    print(respic)
