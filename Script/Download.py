492677e1e90466823e8aa6a6723eb7ce2305c37a
Rokate2023 <77772464+Rokate@users.noreply.github.com>
2023-03-01 18:30:30 +0800

    Update Download.py

--- Script/Download.py
+++ Script/Download.py
@@ -2,7 +2,7 @@ import requests
 import traceback
 
 try:
-    respic = requests.get('https://49152c.com/unite49/h5/picture/detail/latest?pictureTypeId=1087055').json()['data']['largePictureUrl']
+    respic = requests.get('https://4915552c.com/unite49/h5/picture/detail/latest?pictureTypeId=1087055').json()['data']['largePictureUrl']
 
 except Exception as e:
     print('出错了',traceback.format_exc())
