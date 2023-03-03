942dbb02cc5b87a5d24a6a498a26299b23566bc1
Rokate2023 <77772464+Rokate@users.noreply.github.com>
2023-03-03 07:27:32 +0000

    fix

--- Script/Update.py
+++ Script/Update.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 all_file = []
-files = Path('./aomen').rglob('*.*')
+files = Path('/home/runner/work/imagebackup/imagebackup/aomen').rglob('*.*')
 
 for file in files:
     if Path.is_file(file):
