9412747f37daa3ded9c246893d24b83bd40455c9
Rokate2023 <77772464+Rokate@users.noreply.github.com>
2023-05-07 23:14:23 +0800

    Update Download.py

--- Script/Download.py
+++ Script/Download.py
@@ -9,15 +9,15 @@ import re
 async def download_image(session, url, filepath):
     try:
         async with session.get(url) as response:
-            
-             async with aiofiles.open(filepath, "wb") as f:
+           if response.headers["Content-Type"] == "image/jpeg":
+                async with aiofiles.open(filepath, "wb") as f:
                     while True:
                         chunk = await response.content.read(1024)
                         if not chunk:
                             break
                         await f.write(chunk)
-            
-                
+            else:
+                print(filepath + "下载出错了")
     except Exception as e:
         print("下载出错:\t", url, repr(e))
 
@@ -34,7 +34,7 @@ async def main(a, b, c):
         [f"https://www.353583.com/tutu/lhwt{qishu}.jpg", "a-lhwt.jpg"],
         [f"https://www.353583.com/tutu/ugyf{qishu}.jpg", "a-ugyf.jpg"],
         [f"https://49629a.com/img/amhg{qishu}.jpg", "a-amhg.jpg"],
-        [f"https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/zbxyb.jpg", "a-zbxyb.jpg"],
+
         [f"https://49629a.com/img/nm4x8m{qishu}.jpg", "a-nm4x8m.jpg"],
         [f"https://123186a.com/gsbtu/baoma{qishu}.jpg", "a-baoma.jpg"],
         [f"https://123186a.com/gsbtu/hdjr{qishu}.jpg", "a-hdjr.jpg"],
@@ -42,6 +42,12 @@ async def main(a, b, c):
         [f"https://www.353583.com/tutu/ujcc{qishu}.jpg", "a-ujcc.jpg"],
         [f"https://www.29761b.com/img/djpt{qishu}.jpg", "a-djpt.jpg"],
         [f"https://49629a.com/img/1xzt{qishu}.jpg", "a-1xzt.jpg"],
+
+        ["https://67292b.com/tu/f011.jpg", "a-f011.jpg"],
+
+    ]
+"""
+        [f"https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/zbxyb.jpg", "a-zbxyb.jpg"],
         [f"https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/n1.jpg", "a-n1.jpg"],
         [f"https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/dcxj.jpg", "a-dcxj.jpg"],
         [f"https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/sszm.jpg", "a-sszm.jpg"],
@@ -56,7 +62,7 @@ async def main(a, b, c):
         [f"https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/b8.jpg", "a-b8.jpg"],
         [f"https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/tjn.jpg", "a-tjn.jpg"],
         [f"https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/ampt.jpg", "a-ampt.jpg"],
-        ["https://67292b.com/tu/f011.jpg", "a-f011.jpg"],
+        
         [f"https://tk.shuangshuangjieyanw.com:4949/col/{qs}/n1.jpg", "x-n1.jpg"],
         [f"https://tk.shuangshuangjieyanw.com:4949/col/{qs}/b002.jpg", "x-b002.jpg"],
         [f"https://tk.shuangshuangjieyanw.com:4949/col/{qs}/b004.jpg", "x-b004.jpg"],
@@ -64,8 +70,7 @@ async def main(a, b, c):
         [f"https://tk.shuangshuangjieyanw.com:4949/col/{qs}/b008.jpg", "x-b008.jpg"],
         [f"https://tk.shuangshuangjieyanw.com:4949/col/{qs}/j11.jpg", "x-j11.jpg"],
         [f"https://tk.shuangshuangjieyanw.com:4949/col/{qs}/qlb.jpg", "x-qlb.jpg"],
-    ]
-
+"""
     async with aiohttp.ClientSession() as session:
         tasks = []
         for i, url in enumerate(urls):
@@ -169,7 +174,7 @@ if __name__ == "__main__":
         #amcode = {"code": "71"}
         qishu = requests.post('https://am49.app/open/latest?code=71').json()['data']['nextIssueNo'][-3:]
         qish = qishu.lstrip('0')
-        print(qishu,qish)
+        
         #xgcode = {"code": "28"}
         qs = requests.post('https://am49.app/open/latest?code=28').json()['data']['nextIssueNo'][-3:].lstrip('0')
     except Exception as e:
@@ -181,3 +186,17 @@ if __name__ == "__main__":
         asyncio.run(downloadamimg())
         print("xg图片下载。。。")
         asyncio.run(downloadxgimg())
+        print("保存html")
+        temp = """
+<h1 align="center" style="color:red ; font-size:50px">澳门图片</h1><iframe src="https://zhibo.ahntsy.com:777/" height="180" width=100% title="澳门开奖"></iframe><div align="center" class="imgblock"><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/n1.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/n1.jpg" / ><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/rv.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/rv.jpg" / ></div><div align="center" class="imgblock"><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/dcxj.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/dcxj.jpg" / ><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/sszm.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/sszm.jpg" / ></div><div align="center" class="imgblock"><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-faf.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-faf.jpg" / ><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-fgmc.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-fgmc.jpg" / ></div><div align="center" class="imgblock"><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-6i12m.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-6i12m.jpg" / ><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-gd.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-gd.jpg" / ></div><div align="center" class="imgblock"><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-lhwt.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-lhwt.jpg" / ><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-ugyf.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-ugyf.jpg" / ></div><div align="center" class="imgblock"><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/ktzsx.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/ktzsx.jpg" / ><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-amhg.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-amhg.jpg" / ></div><div align="center" class="imgblock"><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/zbxyb.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/zbxyb.jpg" / ><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-nm4x8m.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-nm4x8m.jpg" / ></div><div align="center" class="imgblock"><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-baoma.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-baoma.jpg" / ><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-hdjr.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-hdjr.jpg" / ></div><div align="center" class="imgblock"><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/ampic0.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/ampic0.jpg" / ><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/ampic1.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/ampic1.jpg" / ></div><div align="center" class="imgblock"><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/tt38.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/tt38.jpg" / ><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/amffh.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/amffh.jpg" / ></div>
+<div align="center" class="imgblock"><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/amfql.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/amfql.jpg" / ><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/twqp.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/twqp.jpg" / ></div>
+<div align="center" class="imgblock"><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-jl3x.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-jl3x.jpg" / ><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-ujcc.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-ujcc.jpg" / ></div>
+<div align="center" class="imgblock"><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-djpt.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-djpt.jpg" / ><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-1xzt.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-1xzt.jpg" / ></div>
+<div align="center" class="imgblock"><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/lhlxsm.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/lhlxsm.jpg" / ><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/b8.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/b8.jpg" / ></div>
+<div align="center" class="imgblock"><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/mfpy.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/mfpy.jpg" / ><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-f011.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/a-f011.jpg" / ></div>
+<div align="center" class="imgblock"><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/tjn.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/tjn.jpg" / ><a href="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/ampt.jpg" target="_blank"><img src="https://tk2.shuangshuangjieyanw.com:4949/col/{qish}/ampt.jpg" / ></div><div align="center"><iframe src="https://zhibo.chong0123.com:777/" height="150" width=100% title="香港开奖"></iframe></div><div align="center" class="imgblock"><a href="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/n1.jpg" target="_blank"><img src="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/n1.jpg" /></div><div align="center" class="imgblock"><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/xgpic0.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/xgpic0.jpg" / ><a href="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/xgpic1.jpg" target="_blank"><img src="https://gh-proxy.com/https://raw.githubusercontent.com/Rokate/imagebackup/main/Script/Src/xgpic1.jpg" / ></div><div align="center" class="imgblock"><a href="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/b002.jpg" target="_blank"><img src="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/b002.jpg" / ><a href="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/b004.jpg" target="_blank"><img src="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/b004.jpg" / ></div><div align="center" class="imgblock"><a href="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/b006.jpg" target="_blank"><img src="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/b006.jpg" / ><a href="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/b008.jpg" target="_blank"><img src="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/b008.jpg" / ></div><div align="center" class="imgblock"><a href="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/j11.jpg" target="_blank"><img src="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/j11.jpg" / ><a href="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/qlb.jpg" target="_blank"><img src="https://tk.shuangshuangjieyanw.com:4949/col/{qs}/qlb.jpg" / ></div></body></html>
+"""
+        tempp='<!DOCTYPE html><html><head><meta charset="utf-8"><title>澳门图片</title></head><body><style type="text/css">.imgblock img{width:50%;height:500px;flost:left;}</style>'
+        text=temp.format(qish=qish,qs=qs)
+        with open("./Script/Src/html.txt") as f:
+            f.write(tempp+text)
