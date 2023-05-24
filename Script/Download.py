29bcaecf0ce8ffb0985c58fda430eb6a9e92e451
Rokate2023 <77772464+Rokate@users.noreply.github.com>
2023-05-24 23:58:48 +0800

    Update Download.py

--- Script/Download.py
+++ Script/Download.py
@@ -35,8 +35,6 @@ async def main(a, b, c):
         [f"https://www.353583.com/tutu/lhwt{qishu}.jpg", "a-lhwt.jpg"],
         [f"https://www.353583.com/tutu/ugyf{qishu}.jpg", "a-ugyf.jpg"],
         [f"https://69760b.com/img/amhg{qishu}.jpg", "a-amhg.jpg"],
-        ["https://xam666.003123.work/js8/tu/4.jpg","a-mj.jpg"],
-        ["https://xam666.003123.work/js8/tu/lxx.png","a-shxg.jpg"],
         [f"https://69760b.com/img/nm4x8m{qishu}.jpg", "a-nm4x8m.jpg"],
         [f"http://123186a.com/gsbtu/baoma{qishu}.jpg", "a-baoma.jpg"],
         [f"http://123186a.com/gsbtu/hdjr{qishu}.jpg", "a-hdjr.jpg"],
@@ -60,43 +58,14 @@ async def main(a, b, c):
 
 async def downloadamimg():
     amimg = [
-        "https://amtutu.003123.club/index.php?c=5",
-        "https://amtutu.003123.club/index.php?c=2",
+         ["https://xam666.003123.work/js8/tu/4.jpg","a-mj.jpg"],
+        ["https://xam666.003123.work/js8/tu/lxx.png","a-shxg.png"],
     ]
-    y = 0
-    data = {"password": "003123.com"}
-    for url in amimg:
-        pic = str(y)
-        y = y + 1
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.post(url, data=data) as resp:
-                    page = await resp.text()
-                    pattern = re.compile('Title: <a href="(.*?)">No Title')
-                    m = re.findall(pattern, page)
-                    async with session.get(m[0]) as resp:
-                        page2 = await resp.text()
-                    pattern2 = re.compile('InterPhoto.image.php(.*?)"')
-                    m2 = re.findall(pattern2, page2)
-                    headers = {
-                        "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="98"',
-                        "Referer": m[0],
-                        "sec-ch-ua-mobile": "?0",
-                        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.139 Safari/537.36",
-                        "sec-ch-ua-platform": '"Windows"',
-                    }
-                    picfile = await session.get(
-                        "https://amtutu.003123.club/InterPhoto.image.php" + m2[0],
-                        headers=headers,
-                    )
-                    async with aiofiles.open(f"./Script/Src/ampic{pic}.jpg", "wb") as f:
-                        while True:
-                            chunk = await picfile.content.read(1024)
-                            if not chunk:
-                                break
-                            await f.write(chunk)
-        except Exception as e:
-            print("下载出错:\t", url, repr(e))
+    for i, url in enumerate(amimg):
+        response = requests.get(url[0])
+        file_path = "./Script/Src/" + url[1]
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
 
 
 async def downloadxgimg():
