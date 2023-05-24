7867be68f84fd6615bba2cf7a84120908fa43cb6
Rokate2023 <77772464+Rokate@users.noreply.github.com>
2023-05-24 23:32:25 +0800

    Update Download.py

--- Script/Download.py
+++ Script/Download.py
@@ -35,7 +35,8 @@ async def main(a, b, c):
         [f"https://www.353583.com/tutu/lhwt{qishu}.jpg", "a-lhwt.jpg"],
         [f"https://www.353583.com/tutu/ugyf{qishu}.jpg", "a-ugyf.jpg"],
         [f"https://69760b.com/img/amhg{qishu}.jpg", "a-amhg.jpg"],
-
+        ["https://xam666.003123.work/js8/tu/4.jpg","a-mj.jpg"],
+        ["https://xam666.003123.work/js8/tu/lxx.png","a-shxg.png"],
         [f"https://69760b.com/img/nm4x8m{qishu}.jpg", "a-nm4x8m.jpg"],
         [f"http://123186a.com/gsbtu/baoma{qishu}.jpg", "a-baoma.jpg"],
         [f"http://123186a.com/gsbtu/hdjr{qishu}.jpg", "a-hdjr.jpg"],
