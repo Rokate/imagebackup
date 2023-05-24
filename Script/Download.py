2403048a88b1d96349b0583091916f9295681133
Rokate2023 <77772464+Rokate@users.noreply.github.com>
2023-05-24 23:51:53 +0800

    Update Download.py

--- Script/Download.py
+++ Script/Download.py
@@ -133,7 +133,7 @@ async def downloadxgimg():
                         "sec-ch-ua-platform": '"Windows"',
                     }
                     picfile = await session.get(
-                        "https://xgtutu.003123.club/InterPhoto.image.php" + m2[0],
+                        "https://xgtk.003123.work/InterPhoto.image.php" + m2[0],
                         headers=headers,
                     )
                     async with aiofiles.open(f"./Script/Src/xgpic{pic}.jpg", "wb") as f:
