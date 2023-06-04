825a72b9b88432b7899ff29be36d728d05c20ffa
Rokate <77772464+Rokate@users.noreply.github.com>
2023-06-05 01:26:00 +0800

    Update:  2023.06.05 01:26:00

--- Script/Src/xjw/管家婆解析.js
+++ Script/Src/xjw/管家婆解析.js
@@ -26,8 +26,8 @@ document.writeln("	");
 document.writeln("		<p align=\'left\'>");
 document.writeln("	");
 document.writeln("		<span><font size=\'4\'>156期:<font color=\'#008000\'>【精解管家婆一句话】</font><font color=\'#FF0000\'>开？00</font><font color=\'#008000\'><br>");
-document.writeln("		</font><font color=\'#6600CC\'>【更新中】【更新中】<br></font></font>");
-document.writeln("		<font color=\'#0000FF\' size=\'4\'>解：更新中。综合取肖</font><font color=\'#FF0000\' size=\'4\'>更新中</font><font color=\'#0000FF\' size=\'4\'>   本期平特更新中。<br>本期取码：更新中</td></tr>");
+document.writeln("		</font><font color=\'#6600CC\'>【本期绿红定出特】【亡羊得牛】<br></font></font>");
+document.writeln("		<font color=\'#0000FF\' size=\'4\'>解：本期绿红定出特解料：本期绿红定出特，绿红。解码：绿红。综合取肖</font><font color=\'#FF0000\' size=\'4\'>绿红</font><font color=\'#0000FF\' size=\'4\'>   本期平特羊牛。<br>本期取码：绿红</td></tr>");
 document.writeln("		");
 document.writeln("");
  
