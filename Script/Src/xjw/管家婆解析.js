d89f9b55737cf7f003760a8fe3c0f41cb204d83b
Rokate <77772464+Rokate@users.noreply.github.com>
2023-06-14 03:26:02 +0800

    Update:  2023.06.14 03:26:02

--- Script/Src/xjw/管家婆解析.js
+++ Script/Src/xjw/管家婆解析.js
@@ -27,8 +27,8 @@ document.writeln("	");
 document.writeln("		<p align=\'left\'>");
 document.writeln("	");
 document.writeln("		<span><font size=\'4\'>165期:<font color=\'#008000\'>【精解管家婆一句话】</font><font color=\'#FF0000\'>开？00</font><font color=\'#008000\'><br>");
-document.writeln("		</font><font color=\'#6600CC\'>【更新中】【更新中】<br></font></font>");
-document.writeln("		<font color=\'#0000FF\' size=\'4\'>解：更新中。综合取肖</font><font color=\'#FF0000\' size=\'4\'>更新中</font><font color=\'#0000FF\' size=\'4\'>   本期平特更新中。<br>本期取码：更新中</td></tr>");
+document.writeln("		</font><font color=\'#6600CC\'>【二下五上红波旺】【叶公好龙】<br></font></font>");
+document.writeln("		<font color=\'#0000FF\' size=\'4\'>解：二下五上红波旺解料：二下五上红波旺，解红波。解码：红波。综合取肖</font><font color=\'#FF0000\' size=\'4\'>红波</font><font color=\'#0000FF\' size=\'4\'>   本期平特龙。<br>本期取码：红波</td></tr>");
 document.writeln("		");
 document.writeln("");
 
