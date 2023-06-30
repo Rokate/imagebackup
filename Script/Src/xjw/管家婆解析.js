ce0a8b90a531b18ab149a42ff07ad6408589a700
Rokate <77772464+Rokate@users.noreply.github.com>
2023-07-01 02:26:07 +0800

    Update:  2023.07.01 02:26:07

--- Script/Src/xjw/管家婆解析.js
+++ Script/Src/xjw/管家婆解析.js
@@ -26,8 +26,8 @@ document.writeln("	");
 document.writeln("		<p align=\'left\'>");
 document.writeln("	");
 document.writeln("		<span><font size=\'4\'>182期:<font color=\'#008000\'>【精解管家婆一句话】</font><font color=\'#FF0000\'>开？00</font><font color=\'#008000\'><br>");
-document.writeln("		</font><font color=\'#6600CC\'>【更新中】【更新中】<br></font></font>");
-document.writeln("		<font color=\'#0000FF\' size=\'4\'>解：更新中。综合取肖</font><font color=\'#FF0000\' size=\'4\'>更新中</font><font color=\'#0000FF\' size=\'4\'>   本期平特更新中。<br>本期取码：更新中</td></tr>");
+document.writeln("		</font><font color=\'#6600CC\'>【二七四一再合五】【狗急跳墙】<br></font></font>");
+document.writeln("		<font color=\'#0000FF\' size=\'4\'>解：二七四一再合五，阳性:牛虎兔羊猴鸡。综合取肖</font><font color=\'#FF0000\' size=\'4\'>牛虎兔羊猴鸡</font><font color=\'#0000FF\' size=\'4\'>   本期平特狗。<br>本期取码：27.39.38.02.25.37.33.45.08.32.07.43</td></tr>");
 document.writeln("		");
 document.writeln("");
 
