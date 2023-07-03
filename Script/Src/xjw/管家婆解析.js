97a09d013961a4b31ed100e5b38845d30383e74a
Rokate <77772464+Rokate@users.noreply.github.com>
2023-07-04 02:26:08 +0800

    Update:  2023.07.04 02:26:08

--- Script/Src/xjw/管家婆解析.js
+++ Script/Src/xjw/管家婆解析.js
@@ -25,8 +25,8 @@ document.writeln("	");
 document.writeln("		<p align=\'left\'>");
 document.writeln("	");
 document.writeln("		<span><font size=\'4\'>185期:<font color=\'#008000\'>【精解管家婆一句话】</font><font color=\'#FF0000\'>开？00</font><font color=\'#008000\'><br>");
-document.writeln("		</font><font color=\'#6600CC\'>【更新中】【更新中】<br></font></font>");
-document.writeln("		<font color=\'#0000FF\' size=\'4\'>解：更新中。综合取肖</font><font color=\'#FF0000\' size=\'4\'>更新中</font><font color=\'#0000FF\' size=\'4\'>   本期平特更新中。<br>本期取码：更新中</td></tr>");
+document.writeln("		</font><font color=\'#6600CC\'>【牛头马嘴三九开】【猴年马月】<br></font></font>");
+document.writeln("		<font color=\'#0000FF\' size=\'4\'>解：牛头马嘴三九开，单笔:鼠龙蛇马鸡猪。综合取肖</font><font color=\'#FF0000\' size=\'4\'>鼠龙蛇马鸡猪</font><font color=\'#0000FF\' size=\'4\'>   本期平特猴马。<br>本期取码：40.28.12.48.11.23.34.46.07.19.41.17</td></tr>");
 document.writeln("		");
  
 
