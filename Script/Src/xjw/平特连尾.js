e4d61c7180ca02e5a157682fe502c7a90733d204
Rokate <77772464+Rokate@users.noreply.github.com>
2023-08-18 11:21:20 +0800

    Update:  2023.08.18 11:21:20

--- Script/Src/xjw/平特连尾.js
+++ Script/Src/xjw/平特连尾.js
@@ -24,20 +24,20 @@ document.writeln("");
 document.writeln("<table border=\'1\' width=\'100%\' cellpadding=\'0\' cellspacing=\'0\' bordercolorlight=\'#FFFFFF\' bordercolordark=\'#FFFFFF\' bgcolor=\'#FFFFFF\'>");
 document.writeln("		");
 document.writeln("		");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
 document.writeln("				");
 document.writeln("");
 document.writeln("		<!--开始-->");
@@ -47,8 +47,8 @@ document.writeln("	<b>230期<font color=\'#6600CC\'>【六尾→拖39尾】开</
 document.writeln("		<!--结束-->");
 document.writeln("		");
 document.writeln("");
-
-
+document.writeln("");
+document.writeln("");
 document.writeln("				");
 document.writeln("");
 document.writeln("			<!--开始-->");
@@ -57,21 +57,21 @@ document.writeln("	<td width=100% height=40><p align=center><font size=\'4\' fac
 document.writeln("	<b>229期<font color=\'#6600CC\'>【<span style=\'background-color: #FFFF00\'>八尾</span>→拖<span style=\'background-color: #FFFF00\'>5</span>2尾】开</font><font color=\'#FF6600\'>28.15.45</font>准</b></font></table>");
 document.writeln("		<!--结束-->");
 document.writeln("");
-
-
- 
-
-
+document.writeln("");
+document.writeln("");
+document.writeln(" ");
+document.writeln("");
+document.writeln("");
 document.writeln("				");
 document.writeln("			<!--开始-->");
 document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff><tbody><tr>");
 document.writeln("	<td width=100% height=40><p align=center><font size=\'4\' face=\'楷体\'>");
 document.writeln("	<b>227期<font color=\'#6600CC\'>【<span style=\'background-color: #FFFF00\'>九尾</span>→拖<span style=\'background-color: #FFFF00\'>38</span>尾】开</font><font color=\'#FF6600\'>19.03.38</font>准</b></font></table>");
 document.writeln("		");
-document.writeln("		
-
- 
-
+document.writeln("		");
+document.writeln("");
+document.writeln(" ");
+document.writeln("");
 document.writeln("		<!--开始-->");
 document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff><tbody><tr>");
 document.writeln("	<td width=100% height=40><p align=center><font size=\'4\' face=\'楷体\'>");
@@ -79,8 +79,8 @@ document.writeln("	<b>225期<font color=\'#6600CC\'>【<span style=\'background-
 document.writeln("		<!--结束-->");
 document.writeln("");
 document.writeln("");
-
- 
+document.writeln("");
+document.writeln(" ");
 document.writeln("				");
 document.writeln("");
 document.writeln("		<!--开始-->");
@@ -90,8 +90,8 @@ document.writeln("	<b>223期<font color=\'#6600CC\'>【<span style=\'background-
 document.writeln("		<!--结束-->");
 document.writeln("		");
 document.writeln("");
-
-
+document.writeln("");
+document.writeln("");
 document.writeln("			<!--开始-->");
 document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff><tbody><tr>");
 document.writeln("	<td width=100% height=40><p align=center><font size=\'4\' face=\'楷体\'>");
@@ -99,21 +99,21 @@ document.writeln("	<b>222期<font color=\'#6600CC\'>【<span style=\'background-
 document.writeln("		<!--结束-->");
 document.writeln("");
 document.writeln("");
-
-
- 
-
+document.writeln("");
+document.writeln("");
+document.writeln(" ");
+document.writeln("");
 document.writeln("				<!--开始-->");
 document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff><tbody><tr>");
 document.writeln("	<td width=100% height=40><p align=center><font size=\'4\' face=\'楷体\'>");
 document.writeln("	<b>220期<font color=\'#6600CC\'>【<span style=\'background-color: #FFFF00\'>六尾</span>→拖4<span style=\'background-color: #FFFF00\'>8</span>尾】开</font><font color=\'#FF6600\'>46.18.26</font>准</b></font></table>");
 document.writeln("		<!--结束-->");
 document.writeln("");
-
- 
- 
- 
- 
+document.writeln("");
+document.writeln(" ");
+document.writeln(" ");
+document.writeln(" ");
+document.writeln(" ");
 document.writeln("		");
 document.writeln("<table style=border-collapse:collapse border=0 width=100%><tbody><tr><td height=40><p style=text-align:center><strong>");
 document.writeln("			<a target=\'_blank\' href=\'https://j.100tzz.com:100/\' style=\'text-decoration: none\'><img src=/yyy123.gif style=\'width:20px\' height=\'19\'><span style=font-family:楷体;color:#00f><font style=\'font-size: 15pt\'>点击投注澳门六合彩</font></span><span style=font-family:楷体;font-size:17pt;><font color=\'#FF0000\'>100.cc</font></span></a></strong></tr></table>								");
