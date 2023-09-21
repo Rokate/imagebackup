72615e48381409a04bd55074087ca3a7f34dbfdb
Rokate <77772464+Rokate@users.noreply.github.com>
2023-09-22 03:25:37 +0800

    Update:  2023.09.22 03:25:37

--- Script/Src/xjw/七尾.js
+++ Script/Src/xjw/七尾.js
@@ -32,7 +32,7 @@ document.writeln("							<td class=\'center f11 black l150\' align=\'center\'>")
 document.writeln("		<p>");
 document.writeln("		<b>");
 document.writeln("		<font size=\'4\'>");
-document.writeln("		<span>264期:<font color=\'#008000\'>无错七尾</font>【1245389】</span><font color=\'#FF0000\'>开？00</font></font></b></td></tr>");
+document.writeln("		<span>265期:<font color=\'#008000\'>无错七尾</font>【1245389】</span><font color=\'#FF0000\'>开？00</font></font></b></td></tr>");
 document.writeln("		");
 document.writeln("");
 
