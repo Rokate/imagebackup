9285a1dc10a62be235b0cbc9c0e4797ae7f71d0e
Rokate <77772464+Rokate@users.noreply.github.com>
2023-10-08 22:23:10 +0800

    Update:  2023.10.08 22:23:10

--- Script/Src/xjw/综合特码.js
+++ Script/Src/xjw/综合特码.js
@@ -64,7 +64,11 @@ document.writeln("	");
 
 
 
-document.writeln("281期团队十肖：龙鼠猪蛇猴鸡虎兔狗羊<br>");
+
+
+
+document.writeln("282期团队十肖：蛇牛鼠虎猪兔羊马猴鸡<br>");
+document.writeln("281期团队十肖：龙<span style=\'background-color: #FFFF00\'>鼠</span>猪蛇猴鸡虎兔狗羊<br>");
 document.writeln("280期团队十肖：狗兔鸡猴羊鼠虎<span style=\'background-color: #FFFF00\'>马</span>牛猪<br>");
 document.writeln("279期团队十肖：猴蛇<span style=\'background-color: #FFFF00\'>马</span>猪牛龙兔狗鼠羊<br>");
 document.writeln("278期团队十肖：兔<span style=\'background-color: #FFFF00\'>牛</span>猪羊蛇鸡马猴鼠狗<br>");
@@ -78,8 +82,8 @@ document.writeln("<br>");
 
 
 
-
-document.writeln("281期团队八尾：12456789<br>");
+document.writeln("282期团队八尾：01245689<br>");
+document.writeln("281期团队八尾：1245<span style=\'background-color: #FFFF00\'>6</span>789<br>");
 document.writeln("280期团队八尾：1234<span style=\'background-color: #FFFF00\'>6</span>789<br>");
 document.writeln("279期团队八尾：<span style=\'background-color: #FFFF00\'>0</span>1256789<br>");
 document.writeln("277期团队八尾：123567<span style=\'background-color: #FFFF00\'>8</span>9<br>");
@@ -99,8 +103,7 @@ document.writeln("<br>");
 
 
 
-
-document.writeln("281期团队四头：0234<br>");
+document.writeln("282期团队四头：2134<br>");
 document.writeln("280期团队四头：012<span style=\'background-color: #FFFF00\'>4</span><br>");
 document.writeln("279期团队四头：0<span style=\'background-color: #FFFF00\'>1</span>23<br>");
 document.writeln("277期团队四头：<span style=\'background-color: #FFFF00\'>1</span>234<br>");
@@ -121,8 +124,8 @@ document.writeln("");
 
 
 
-
-document.writeln("281期团队四行：金水火土<br>");
+document.writeln("282期团队四行：金木水土<br>");
+document.writeln("281期团队四行：金水<span style=\'background-color: #FFFF00\'>火</span>土<br>");
 document.writeln("280期团队四行：金水<span style=\'background-color: #FFFF00\'>火</span>土<br>");
 document.writeln("279期团队四行：<span style=\'background-color: #FFFF00\'>金</span>木水火<br>");
 document.writeln("278期团队四行：<span style=\'background-color: #FFFF00\'>金</span>水火土<br>");
