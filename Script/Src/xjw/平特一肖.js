0f5e9a5d75519b01a23e0755bb5f9d8d049189ac
Rokate <77772464+Rokate@users.noreply.github.com>
2023-10-18 23:21:11 +0800

    Update:  2023.10.18 23:21:11

--- Script/Src/xjw/平特一肖.js
+++ Script/Src/xjw/平特一肖.js
@@ -26,13 +26,13 @@ document.writeln("	   		");
 
 
 
-
+document.writeln("	");
 document.writeln("			    <tr>");
 document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
-document.writeln("		<span><font size=\'4\'>291期:【歇后语解平特】开<span style=\'background-color: #FFFF00\'>00</span><br>");
-document.writeln("		<font color=\'#FF00FF\'>【狗吠月亮—少见多怪】</td>");
+document.writeln("		<span><font size=\'4\'>292期:【歇后语解平特】开<span style=\'background-color: #FFFF00\'>00</span><br>");
+document.writeln("		<font color=\'#FF00FF\'>【鸡给黄鼠狼拜年—自投罗网】</td>");
 document.writeln("		</tr>");
-document.writeln("");
+document.writeln("		");
 
 
 document.writeln("	");
