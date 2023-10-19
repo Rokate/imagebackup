29cdf29b227ed7faab6542c5e6f2b6f21f081c56
Rokate <77772464+Rokate@users.noreply.github.com>
2023-10-19 23:21:07 +0800

    Update:  2023.10.19 23:21:07

--- Script/Src/xjw/七尾.js
+++ Script/Src/xjw/七尾.js
@@ -29,16 +29,16 @@ document.writeln("<table border=\'1\' width=\'100%\' cellpadding=\'0\' cellspaci
 
 
 
-document.writeln("	");
-document.writeln("							<td class=\'center f11 black l150\' align=\'center\'>");
+document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
 document.writeln("		<p>");
 document.writeln("		<b>");
 document.writeln("		<font size=\'4\'>");
-document.writeln("		<span>292期:<font color=\'#008000\'>无错七尾</font>【0127389】</span><font color=\'#FF0000\'>开？00</font></font></b></td></tr>");
+document.writeln("		<span>293期:<font color=\'#008000\'>无错七尾</font>【1245863】</span><font color=\'#FF0000\'>开？00</font></font></b></td></tr>");
 document.writeln("		");
 document.writeln("");
 document.writeln("");
-document.writeln("");
+
+
 
 document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
 document.writeln("		<p>");
--- Script/Src/xjw/三肖三码.js
+++ Script/Src/xjw/三肖三码.js
@@ -17,6 +17,16 @@ document.writeln("		");
 
 
 
+document.writeln("");
+document.writeln("				<tr>");
+document.writeln("		<td class=\'center f11 black l150\'>");
+document.writeln("	");
+document.writeln("		<p align=\'center\'>");
+document.writeln("	");
+document.writeln("		<span style=\'vertical-align: middle\'><font size=\'4\'>293期:<font color=\'#FF00FF\'>《猪马羊》</font><font color=\'#008000\'>（05 46 21）</font><font color=\'#0000FF\'>开？00中</font></font></span></td></tr>");
+document.writeln("	");
+document.writeln("");
+document.writeln("");
 
 document.writeln("");
 document.writeln("				<tr>");
@@ -24,7 +34,7 @@ document.writeln("		<td class=\'center f11 black l150\'>");
 document.writeln("	");
 document.writeln("		<p align=\'center\'>");
 document.writeln("	");
-document.writeln("		<span style=\'vertical-align: middle\'><font size=\'4\'>292期:<font color=\'#FF00FF\'>《虎兔鸡》</font><font color=\'#008000\'>（26 49 31）</font><font color=\'#0000FF\'>开？00中</font></font></span></td></tr>");
+document.writeln("		<span style=\'vertical-align: middle\'><font size=\'4\'>292期:<font color=\'#FF00FF\'>《虎<span style=\'background-color: #FFFF00\'>兔</span>鸡》</font><font color=\'#008000\'>（26 49 31）</font><font color=\'#0000FF\'>开兔25中</font></font></span></td></tr>");
 document.writeln("	");
 document.writeln("		");
 document.writeln("");
