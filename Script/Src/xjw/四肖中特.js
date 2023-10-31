744160010fdd7c53cdc257836062e11b3e9c6c2f
Rokate <77772464+Rokate@users.noreply.github.com>
2023-10-31 22:22:03 +0800

    Update:  2023.10.31 22:22:03

--- Script/Src/xjw/四肖中特.js
+++ Script/Src/xjw/四肖中特.js
@@ -33,32 +33,19 @@ document.writeln("");
 
 
 
-document.writeln("	<tr>");
-document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
-document.writeln("	");
-document.writeln("		<span><font size=\'4\'>304期:<font color=\'#008000\'>【四肖中特诗】</font><font color=\'#FF0000\'>开？00</font><font color=\'#008000\'><br>");
-document.writeln("		</font><font color=\'#6600CC\'>【棋逢敌手方可喜，虎狗兔猪是特码】<br></font></font>");
-document.writeln("		<font color=\'#0000FF\' size=\'4\'>解：</font></span><font size=\'4\' color=\'#0000FF\'>虎狗兔猪</font></tr>");
-document.writeln("");
-document.writeln("");
-document.writeln("");
-
-
-
-
-document.writeln("");
 document.writeln("");
 document.writeln("	<tr>");
 document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
 document.writeln("	");
-document.writeln("		<span><font size=\'4\'>301期:<font color=\'#008000\'>【四肖中特诗】</font><font color=\'#FF0000\'>开兔25</font><font color=\'#008000\'><br>");
-document.writeln("		</font><font color=\'#6600CC\'>【看中羊头两只角，马鸡兔猪是特码】<br></font></font>");
-document.writeln("		<font color=\'#0000FF\' size=\'4\'>解：</font></span><font size=\'4\' color=\'#0000FF\'>马鸡<span style=\'background-color: #FFFF00\'>兔</span>猪</font></tr>");
+document.writeln("		<span><font size=\'4\'>305期:<font color=\'#008000\'>【四肖中特诗】</font><font color=\'#FF0000\'>开？00</font><font color=\'#008000\'><br>");
+document.writeln("		</font><font color=\'#6600CC\'>【金玉水灵尘地间，鸡猪龙马是特码】<br></font></font>");
+document.writeln("		<font color=\'#0000FF\' size=\'4\'>解：</font></span><font size=\'4\' color=\'#0000FF\'>鸡猪龙马</font></tr>");
 document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("");
-document.writeln("");
+
+
 
 
 
--- Script/Src/xjw/综合特码.js
+++ Script/Src/xjw/综合特码.js
@@ -67,8 +67,7 @@ document.writeln("	");
 
 
 
-
-document.writeln("304期团队十肖：牛鸡狗马虎蛇猴兔龙羊<br>");
+document.writeln("305期团队十肖：鸡鼠兔猴马羊虎狗龙蛇<br>");
 document.writeln("303期团队十肖：鼠猴蛇羊虎狗兔龙<span style=\'background-color: #FFFF00\'>牛</span>鸡<br>");
 document.writeln("302期团队十肖：龙牛马羊虎猪鼠鸡<span style=\'background-color: #FFFF00\'>猴</span>蛇<br>");
 document.writeln("301期团队十肖：鼠猴马羊虎狗<span style=\'background-color: #FFFF00\'>兔</span>猪龙鸡<br>");
@@ -88,8 +87,8 @@ document.writeln("<br>");
 
 
 
-
-document.writeln("304期团队八尾：01245789<br>");
+document.writeln("305期团队八尾：12356789<br>");
+document.writeln("304期团队八尾：012457<span style=\'background-color: #FFFF00\'>8</span>9<br>");
 document.writeln("303期团队八尾：0123<span style=\'background-color: #FFFF00\'>5</span>679<br>");
 document.writeln("301期团队八尾：0134<span style=\'background-color: #FFFF00\'>5</span>678<br>");
 document.writeln("300期团队八尾：02345<span style=\'background-color: #FFFF00\'>6</span>89<br>");
@@ -132,8 +131,8 @@ document.writeln("<br>");
 
 
 
-
-document.writeln("304期团队四头：0234<br>");
+document.writeln("305期团队四头：1234<br>");
+document.writeln("304期团队四头：0<span style=\'background-color: #FFFF00\'>2</span>34<br>");
 document.writeln("303期团队四头：0<span style=\'background-color: #FFFF00\'>1</span>23<br>");
 document.writeln("302期团队四头：012<span style=\'background-color: #FFFF00\'>3</span><br>");
 document.writeln("301期团队四头：01<span style=\'background-color: #FFFF00\'>2</span>4<br>");
@@ -159,8 +158,8 @@ document.writeln("");
 
 
 
-
-document.writeln("304期团队四行：木水火土<br>");
+document.writeln("305期团队四行：金木火土<br>");
+document.writeln("304期团队四行：木<span style=\'background-color: #FFFF00\'>水</span>火土<br>");
 document.writeln("303期团队四行：金木水<span style=\'background-color: #FFFF00\'>火</span><br>");
 document.writeln("301期团队四行：金木火<span style=\'background-color: #FFFF00\'>土</span><br>");
 document.writeln("299期团队四行：<span style=\'background-color: #FFFF00\'>金</span>木火土<br>");
