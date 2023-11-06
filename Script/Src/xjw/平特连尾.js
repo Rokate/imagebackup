2f420ab2a0b7c26ff7d505ce6ad346556ce856fa
Rokate <77772464+Rokate@users.noreply.github.com>
2023-11-06 23:21:03 +0800

    Update:  2023.11.06 23:21:03

--- Script/Src/xjw/平特连尾.js
+++ Script/Src/xjw/平特连尾.js
@@ -59,7 +59,7 @@ document.writeln("		<!--结束-->");
 document.writeln("		        <!--开始-->");
 document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff><tbody><tr>");
 document.writeln("	<td width=100% height=40><p align=center><font size=\'4\' face=\'楷体\'>");
-document.writeln("	<b>308期<font color=\'#6600CC\'>【<span style=\'background-color: #FFFF00\'>六尾</span>→拖span style=\'background-color: #FFFF00\'>29<</span>尾】开</font><font color=\'#FF6600\'>16.22.19</font>准</b></font></table>");
+document.writeln("	<b>308期<font color=\'#6600CC\'>【<span style=\'background-color: #FFFF00\'>六尾</span>→拖<span style=\'background-color: #FFFF00\'>29<</span>尾】开</font><font color=\'#FF6600\'>16.22.19</font>准</b></font></table>");
 document.writeln("		<!--结束-->");
 document.writeln("		");
 
