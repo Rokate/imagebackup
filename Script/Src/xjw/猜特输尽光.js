5ce55ba8cd31525d54709756b7f71b687fea3b1d
Rokate <77772464+Rokate@users.noreply.github.com>
2023-12-19 13:21:16 +0800

    Update:  2023.12.19 13:21:16

--- Script/Src/xjw/猜特输尽光.js
+++ Script/Src/xjw/猜特输尽光.js
@@ -18,7 +18,7 @@ document.writeln("	<tr>");
 document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
 document.writeln("	");
 document.writeln("		<span><font size=\'4\'>353期:<font color=\'#008000\'>【正版猜特输尽光】</font><font color=\'#FF0000\'>开？00</font><font color=\'#008000\'><br>");
-document.writeln("		</font><font color=\'#FF00FF\'>今期买鼠输尽光，东船西航两头现。 </font></td>");
+document.writeln("		</font><font color=\'#FF00FF\'>今期鼠猪输尽光，一头三尾全不买。 </font></td>");
 document.writeln("		</tr>");
 document.writeln("");
 document.writeln("");
