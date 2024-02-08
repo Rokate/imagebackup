2cc1229f4d6bd1c4d99b8855186237fc3241a94c
Rokate <77772464+Rokate@users.noreply.github.com>
2024-02-09 01:21:01 +0800

    Update:  2024.02.09 01:21:01

--- Script/Src/xjw/管家婆解析.js
+++ Script/Src/xjw/管家婆解析.js
@@ -27,8 +27,17 @@ document.writeln("<table border=\'1\' width=\'100%\' cellpadding=\'0\' cellspaci
 
 document.writeln("<tr><td class=\'center f11 black l150\' align=\'center\'>");
 document.writeln("<p align=\'left\'>");
-document.writeln("<span><font size=\'4\'>039期:<font color=\'#008000\'>【精解管家婆一句赢大钱】</font><font color=\'#FF0000\'>开？00</font><font color=\'#008000\'><br>");
-document.writeln("</font><font color=\'#6600CC\'>精解九肖：鼠龙蛇马狗猪羊猴鸡<br></font></font>");
+document.writeln("<span><font size=\'4\'>040期:<font color=\'#008000\'>【精解管家婆一句赢大钱】</font><font color=\'#FF0000\'>开？00</font><font color=\'#008000\'><br>");
+document.writeln("</font><font color=\'#6600CC\'>精解九肖：兔龙蛇马羊猴鼠牛虎<br></font></font>");
+document.writeln("<font color=\'#FF0000\' size=\'4\'>精解18码：</font><font color=\'#0000FF\' size=\'4\'> 25.37.24.36.11.23.34.46.33.45.08.32.28.16.39.27.14.38<br></font>");
+document.writeln("</td></tr>");
+document.writeln("");
+document.writeln("");
+
+document.writeln("<tr><td class=\'center f11 black l150\' align=\'center\'>");
+document.writeln("<p align=\'left\'>");
+document.writeln("<span><font size=\'4\'>039期:<font color=\'#008000\'>【精解管家婆一句赢大钱】</font><font color=\'#FF0000\'>开鸡19</font><font color=\'#008000\'><br>");
+document.writeln("</font><font color=\'#6600CC\'>精解九肖：鼠龙蛇马狗猪羊猴<span style=\'background-color: #FFFF00\'>鸡</span><br></font></font>");
 document.writeln("<font color=\'#FF0000\' size=\'4\'>精解18码：</font><font color=\'#0000FF\' size=\'4\'>40.28.12.48.11.23.34.46.18.30.05.17.33.45.08.32.07.43<br></font>");
 document.writeln("</td></tr>");
 
