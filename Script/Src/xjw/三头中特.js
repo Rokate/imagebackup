ac6e35e107ef13e8dbceab2ea2f6ba9e2ac2826d
Rokate <77772464+Rokate@users.noreply.github.com>
2024-03-03 21:20:57 +0800

    Update:  2024.03.03 21:20:57

--- Script/Src/xjw/三头中特.js
+++ Script/Src/xjw/三头中特.js
@@ -1,97 +1,16 @@
-﻿document.writeln("");
-document.writeln("</head>");
-document.writeln("");
-document.writeln("<head>");
-document.writeln("<style type=\'text/css\'>");
-document.writeln(".stylee {");
-document.writeln("	background-color: #FFFF00;");
-document.writeln("}");
-document.writeln("</style>");
-document.writeln("</head>");
-document.writeln("");
-document.writeln("");
-document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
-document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
-document.writeln("<b><font face=\'楷体\' style=\'font-size: 20pt\'><font color=\'#FFFF00\'>澳门玄机86563</font><font color=\'#FFFFFF\'>『三头中特』</font></font></b></table>");
-document.writeln("");
-document.writeln("");
-document.writeln("<table border=\'1\' width=\'100%\' style=\'box-sizing: border-box; border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 0px; color: rgb(51, 51, 51); font-family: &quot;Helvetica Neue&quot;, Helvetica, &quot;PingFang SC&quot;, &quot;Hiragino Sans GB&quot;, &quot;Microsoft YaHei&quot;, 微软雅黑, Arial, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial;\'>");
-document.writeln("	<tbody style=\'box-sizing: border-box;\'>");
-document.writeln("	");
-
-
-
-document.writeln("		<tr style=\'box-sizing: border-box;\'>");
-document.writeln("			<td height=\'40\' style=\'box-sizing: border-box;\'>");
-document.writeln("			<p align=\'center\' style=\'box-sizing: border-box; padding: 0px; margin: 0px;\'>");
-document.writeln("			<font size=\'4\' style=\'margin: 0px; padding: 0px;\'>");
-document.writeln("			063期<font color=\'#008000\'>三头中特</font>『<font color=\'#FF0000\'>1.2.4头</font>』<font color=\'#0000FF\'>开？00准</font></font></td>");
-document.writeln("		</tr>");
-document.writeln("");
-document.writeln("");
-document.writeln("");
-
-
-
-document.writeln("				<tr style=\'box-sizing: border-box;\'>");
-document.writeln("			<td height=\'40\' style=\'box-sizing: border-box;\'>");
-document.writeln("			<p align=\'center\' style=\'box-sizing: border-box; padding: 0px; margin: 0px;\'>");
-document.writeln("			<font size=\'4\' style=\'margin: 0px; padding: 0px;\'>");
-document.writeln("			062期<font color=\'#008000\'>三头中特</font>『<font color=\'#FF0000\'>0.3.<span style=\'background-color: #FFFF00\'>4</span>头</font>』<font color=\'#0000FF\'>开龙49准</font></font></td>");
-document.writeln("		</tr>");
-document.writeln("				");
-document.writeln("		");
-document.writeln("");
-document.writeln("");
-document.writeln("");
-
-
-
-
-
-document.writeln("		<tr style=\'box-sizing: border-box;\'>");
-document.writeln("			<td height=\'40\' style=\'box-sizing: border-box;\'>");
-document.writeln("			<p align=\'center\' style=\'box-sizing: border-box; padding: 0px; margin: 0px;\'>");
-document.writeln("			<font size=\'4\' style=\'margin: 0px; padding: 0px;\'>");
-document.writeln("			059期<font color=\'#008000\'>三头中特</font>『<font color=\'#FF0000\'>2.<span style=\'background-color: #FFFF00\'>3</span>.4头</font>』<font color=\'#0000FF\'>开猪30准</font></font></td>");
-document.writeln("		</tr>");
-document.writeln("");
-document.writeln("			");
-document.writeln("");
-document.writeln("");
-
-
-document.writeln("				<tr style=\'box-sizing: border-box;\'>");
-document.writeln("			<td height=\'40\' style=\'box-sizing: border-box;\'>");
-document.writeln("			<p align=\'center\' style=\'box-sizing: border-box; padding: 0px; margin: 0px;\'>");
-document.writeln("			<font size=\'4\' style=\'margin: 0px; padding: 0px;\'>");
-document.writeln("			058期<font color=\'#008000\'>三头中特</font>『<font color=\'#FF0000\'><span style=\'background-color: #FFFF00\'>1</span>.3.4头</font>』<font color=\'#0000FF\'>开马11准</font></font></td>");
-document.writeln("		</tr>");
-document.writeln("				");
-document.writeln("		");
-document.writeln("");
-document.writeln("");
-document.writeln("");
-
-
-document.writeln("			<tr style=\'box-sizing: border-box;\'>");
-document.writeln("			<td height=\'40\' style=\'box-sizing: border-box;\'>");
-document.writeln("			<p align=\'center\' style=\'box-sizing: border-box; padding: 0px; margin: 0px;\'>");
-document.writeln("			<font size=\'4\' style=\'margin: 0px; padding: 0px;\'>");
-document.writeln("			057期<font color=\'#008000\'>三头中特</font>『<font color=\'#FF0000\'>1.2.<span style=\'background-color: #FFFF00\'>4</span>头</font>』<font color=\'#0000FF\'>开羊46准</font></font></td>");
-document.writeln("		</tr>");
-document.writeln("				");
-document.writeln("");
-document.writeln("");
-
-
-
-
-
- 
- 
-
-document.writeln("		");
-document.writeln("<table style=border-collapse:collapse border=0 width=100%><tbody><tr><td height=40><p style=text-align:center><strong>");
-document.writeln("			<a target=\'_blank\' href=\'https://76891.com/#868627\' style=\'text-decoration: none\'><img src=/yyy123.gif style=\'width:20px\' height=\'19\'><span style=font-family:楷体;color:#00f><font style=\'font-size: 15pt\'>进入澳彩网</font></span><span style=font-family:楷体;font-size:17pt;><font color=\'#FF0000\'>76891.com</font></span></a></strong></tr></table>		");
-document.writeln("			</table>");
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>403 Forbidden</title>
+</head>
+<body>
+<div id='main'>
+<i><h2>Something error:</h2></i>
+<p><h3>403 Forbidden</h3><h3><font color='red'>access denied by response control</font></h3></p>
+<p>Please check or <a href='javascript:location.reload()'>try again</a> later.</p>
+<hr>
+<div id='pb'>Generated by <a href='https://www.cdnbest.com/?code=403' target=_blank>kangle/3.5.14</a>.
+</div>
+</div>
+<!-- padding for ie --><!-- padding for ie --><!-- padding for ie --><!-- padding for ie -->
+</body></html>
