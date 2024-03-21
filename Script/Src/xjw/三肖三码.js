cad8055aae2b4118928a37b558c4aaff8629cab3
Rokate <77772464+Rokate@users.noreply.github.com>
2024-03-21 22:21:11 +0800

    Update:  2024.03.21 22:21:11

--- Script/Src/xjw/三肖三码.js
+++ Script/Src/xjw/三肖三码.js
@@ -17,30 +17,16 @@ document.writeln("		");
 
 
 
+
 document.writeln("");
-document.writeln("		");
-document.writeln("				<tr>");
+document.writeln("						<tr>");
 document.writeln("		<td class=\'center f11 black l150\'>");
-document.writeln("	");
 document.writeln("		<p align=\'center\'>");
 document.writeln("	");
-document.writeln("		<span style=\'vertical-align: middle\'><font size=\'4\'>081期:<font color=\'#FF00FF\'>《虎猪猴》</font><font color=\'#008000\'>（15 42 09）</font><font color=\'#0000FF\'>开？00中</font></font></span></td></tr>");
-document.writeln("			");
-document.writeln("		");
+document.writeln("		<span style=\'vertical-align: middle\'><font size=\'4\'>082期:<font color=\'#FF00FF\'>《狗羊兔》</font><font color=\'#008000\'>（07 22 38）</font><font color=\'#0000FF\'>开？00中</font></font></span></div>	");
+document.writeln("	");
 document.writeln("");
-
-
-document.writeln("	");
-document.writeln("				<tr>");
-document.writeln("		<td class=\'center f11 black l150\'>");
-document.writeln("	");
-document.writeln("		<p align=\'center\'>");
-document.writeln("	");
-document.writeln("		<span style=\'vertical-align: middle\'><font size=\'4\'>079期:<font color=\'#FF00FF\'>《虎兔<span style=\'background-color: #FFFF00\'>鸡</span>》</font><font color=\'#008000\'>（27 02 32）</font><font color=\'#0000FF\'>开鸡44中</font></font></span></td></tr>");
-document.writeln("	");
-document.writeln("	");
-
-
+document.writeln("");
 
 
 
