18b50d7434c6faaf32ed884012b7b9dfe891f3da
Rokate <77772464+Rokate@users.noreply.github.com>
2024-05-15 13:20:55 +0800

    Update:  2024.05.15 13:20:55

--- Script/Src/xjw/2qpt.js
+++ Script/Src/xjw/2qpt.js
@@ -48,6 +48,25 @@ document.writeln("");
 
 
 
+
+document.writeln("	<!----开始---->    ");
+document.writeln("	<table border=\'1\' width=\'100%\' cellpadding=\'0\' height=\'83\' cellspacing=\'0\' bgcolor=\'#FFFFFF\' bordercolor=\'#D4D4D4\' style=\'border-collapse: collapse\'>");
+document.writeln("		<tr>");
+document.writeln("		<td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' width=\'10%\'>");
+document.writeln("		<b><font face=\'微软雅黑\'>136-138期</font></b></td>");
+document.writeln("		<td align=\'center\' bgcolor=\'#FFFFFF\' width=\'65%\' rowspan=\'3\'>");
+document.writeln("		<strong>");
+document.writeln("		<font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'><span style='background-color: #FFFF00\'><span style='background-color: #FFFF00\'><span style='background-color: #FFFF00\'>龙</span></span></span><br>（01.13.25）</font></strong></td>");
+document.writeln("		<td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' width=\'10%\'>");
+document.writeln("		<font face=\'微软雅黑\'>000期中</font></td>");
+document.writeln("		</tr>");
+document.writeln("");
+document.writeln("");
+document.writeln("		</table>");
+document.writeln("		");
+document.writeln("");
+
+
 document.writeln("	<!----开始---->    ");
 document.writeln("	<table border=\'1\' width=\'100%\' cellpadding=\'0\' height=\'83\' cellspacing=\'0\' bgcolor=\'#FFFFFF\' bordercolor=\'#D4D4D4\' style=\'border-collapse: collapse\'>");
 document.writeln("		<tr>");
