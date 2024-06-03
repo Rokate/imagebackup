0023edafa404861a4d7cc692133f9970d75a1c57
Rokate <77772464+Rokate@users.noreply.github.com>
2024-06-04 00:20:35 +0800

    Update:  2024.06.04 00:20:35

--- Script/Src/xjw/欲钱料.js
+++ Script/Src/xjw/欲钱料.js
@@ -47,12 +47,22 @@ document.writeln("");
 
 
 
+
+document.writeln("");
+document.writeln("												<tr>");
+document.writeln("		<td class=\'center f11 black l150\'>");
+document.writeln("			<p align=\'center\'>");
+document.writeln("			<span style=\'vertical-align: middle\'><font size=\'4\'>156期:<font color=\'#FF00FF\'>【欲钱买温顺乖巧的生肖】</font><font color=\'#0000FF\'>开？00中</font></td></tr>");
+document.writeln("");
+
+
+
 document.writeln("");
 document.writeln("");
 document.writeln("												<tr>");
 document.writeln("		<td class=\'center f11 black l150\'>");
 document.writeln("			<p align=\'center\'>");
-document.writeln("			<span style=\'vertical-align: middle\'><font size=\'4\'>155期:<font color=\'#FF00FF\'>【欲钱买家中卖力的生肖】</font><font color=\'#0000FF\'>开？00中</font></td></tr>");
+document.writeln("			<span style=\'vertical-align: middle\'><font size=\'4\'>155期:<font color=\'#FF00FF\'>【欲钱买家中卖力的生肖】</font><font color=\'#0000FF\'>开鸡44中</font></td></tr>");
 document.writeln("");
 document.writeln("		");
 
