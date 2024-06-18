e79bec45c993ec50122cd5997f138506e5877d59
Rokate <77772464+Rokate@users.noreply.github.com>
2024-06-18 13:20:37 +0800

    Update:  2024.06.18 13:20:37

--- Script/Src/xjw/3q4x8m.js
+++ Script/Src/xjw/3q4x8m.js
@@ -49,7 +49,7 @@ document.writeln("		<td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' widt
 document.writeln("		<b><font face=\'微软雅黑\'>171期</font></b></td>");
 document.writeln("		<td align=\'center\' bgcolor=\'#FFFFFF\' width=\'65%\' rowspan=\'3\'>");
 document.writeln("		<strong>");
-document.writeln("		<font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'>蛇狗猴鼠</span></font>");
+document.writeln("		<font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'>蛇狗<span style='background-color: #FFFF00\'猴</span>鼠</span></font>");
 document.writeln("		<font face=\'微软雅黑\' color=\'#FF0000\'>（36.48.07.31.09.21.05.41）</span></font></strong><font face=\'微软雅黑\' color=\'#FF0000\'></span></font></td>");
 document.writeln("		<td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' width=\'10%\'>");
 document.writeln("		<font face=\'微软雅黑\'>开:0000</font></td>");
@@ -68,7 +68,7 @@ document.writeln("		<b style=\'padding: 0px; margin: 0px; word-wrap: break-word;
 document.writeln("		<font face=\'微软雅黑\' style=\'word-wrap: break-word; margin: 0px; padding: 0px\'>");
 document.writeln("		169期</font></b></td>");
 document.writeln("		<td align=\'center\' bgcolor=\'#FFFFFF\' width=\'10%\' style=\'height: 26px\'>");
-document.writeln("		<font face=\'微软雅黑\'>开:0000</font></td>");
+document.writeln("		<font face=\'微软雅黑\'>开:猴45</font></td>");
 document.writeln("		</tr>");
 document.writeln("		</table>");
 document.writeln("		");
