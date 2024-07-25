89496b92e474a227a40287cf1c26e16fc28e0cbe
Rokate <77772464+Rokate@users.noreply.github.com>
2024-07-26 07:20:31 +0800

    Update:  2024.07.26 07:20:31

---
+++ Script/Src/xjw/综合特码.js
@@ -0,0 +1,218 @@
+﻿document.writeln("<head>");
+document.writeln("<meta http-equiv=\'Content-Language\' content=\'zh-cn\'>");
+document.writeln("<meta http-equiv=\'Content-Type\' content=\'text/html; charset=utf-8\'>");
+document.writeln("</head>");
+document.writeln("");
+document.writeln("");
+document.writeln("<style type=\'text/css\'>");
+document.writeln(".stylei {");
+document.writeln("	color: #FFFF00;");
+document.writeln("	background-color: #000000;");
+document.writeln("}");
+document.writeln(".stylei {");
+document.writeln("	font-size: medium;");
+document.writeln("	color: #FFFF00;");
+document.writeln("	background-color: #000000;");
+document.writeln("}");
+document.writeln(".stylezhtw {");
+document.writeln("	background-color: #FFFF00;");
+document.writeln("}");
+document.writeln(".style1js547 {");
+document.writeln("	background-color: #FFFF00;");
+document.writeln("	color: #FF0000;");
+document.writeln("}");
+document.writeln("</style>");
+document.writeln("</head>");
+document.writeln("");
+document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
+document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
+document.writeln("<b><font face=\'楷体\' style=\'font-size: 20pt\'><font color=\'#FFFF00\'>战澳团队</font><font color=\'#FFFFFF\'>『综合特码』</font></font></b></table>");
+document.writeln("");
+document.writeln("");
+document.writeln("<script language=\'JavaScript\' type=\'text/javascript\' src=\'/chajiansb.js\' charset=\'utf-8\'></script>");
+document.writeln("");
+document.writeln("");
+document.writeln("<table border=\'1\' width=\'100%\' style=\'box-sizing: border-box; border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 0px; color: rgb(51, 51, 51); font-family: &quot;Helvetica Neue&quot;, Helvetica, &quot;PingFang SC&quot;, &quot;Hiragino Sans GB&quot;, &quot;Microsoft YaHei&quot;, 微软雅黑, Arial, sans-serif; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial;\'>");
+document.writeln("	<tbody style=\'box-sizing: border-box;\'>");
+document.writeln(" ");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("<!--开始-->");
+document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff><tbody><tr>");
+document.writeln("	<td width=100% height=40><p align=left><font face=\'楷体\' size=\'4\'><b>");
+document.writeln("	<font color=\'#0000FF\'>【战澳团队━以\'稳\'闻名】</font></b></font></table>");
+document.writeln("");
+document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff><tbody><tr>");
+document.writeln("	<td width=100% height=40><p align=left><b><font size=\'4\' face=\'楷体\'>");
+document.writeln("	");
+
+
+
+
+
+
+
+
+document.writeln("208期团队十肖：兔牛猪羊蛇鸡马猴鼠狗<br>");
+document.writeln("206期团队十肖：牛鸡狗马虎蛇猴<span style='background-color: #FFFF00\'>兔</span>龙羊<br>");
+document.writeln("205期团队十肖：鼠猴蛇羊虎狗兔龙<span style='background-color: #FFFF00\'>牛</span>鸡<br>");
+document.writeln("204期团队十肖：<span style='background-color: #FFFF00\'>鼠</span>猴蛇羊虎狗兔龙牛鸡<br>");
+document.writeln("203期团队十肖：鼠<span style='background-color: #FFFF00\'>猴</span>马羊虎狗兔猪龙鸡<br>");
+document.writeln("202期团队十肖：鼠猴蛇马虎<span style='background-color: #FFFF00\'>狗</span>兔猪牛鸡<br>");
+document.writeln("201期团队十肖：鼠猴马羊虎狗兔<span style='background-color: #FFFF00\'>龙</span>牛鸡<br>");
+document.writeln("200期团队十肖：鼠蛇马羊虎<span style='background-color: #FFFF00\'>狗</span>猪龙牛鸡<br>");
+document.writeln("198期团队十肖：<span style='background-color: #FFFF00\'>牛</span>兔猪猴马羊鸡狗鼠龙<br>");
+document.writeln("197期团队十肖：蛇牛鼠虎猪兔<span style='background-color: #FFFF00\'>羊</span>马猴鸡<br>");
+document.writeln("196期团队十肖：龙鼠猪蛇猴鸡<span style='background-color: #FFFF00\'>虎</span>兔狗羊<br>");
+document.writeln("195期团队十肖：兔牛猪羊蛇鸡马<span style='background-color: #FFFF00\'>猴</span>鼠狗<br>");
+document.writeln("194期团队十肖：鸡<span style='background-color: #FFFF00\'>鼠</span>兔猴马羊虎狗龙蛇<br>");
+document.writeln("193期团队十肖：<span style='background-color: #FFFF00\'>牛</span>鸡狗马虎蛇猴兔龙羊<br>");
+document.writeln("192期团队十肖：鼠猴蛇羊虎<span style='background-color: #FFFF00\'>狗</span>兔龙牛鸡<br>");
+document.writeln("191期团队十肖：鼠猴蛇羊<span style='background-color: #FFFF00\'>虎</span>狗兔龙牛鸡<br>");
+document.writeln("190期团队十肖：<span style='background-color: #FFFF00\'>鼠</span>猴马羊虎狗兔猪龙鸡<br>");
+document.writeln("189期团队十肖：鼠猴蛇马虎<span style='background-color: #FFFF00\'>狗</span>兔猪牛鸡<br>");
+document.writeln("188期团队十肖：鼠猴马<span style='background-color: #FFFF00\'>羊</span>虎狗兔龙牛鸡<br>");
+
+
+document.writeln("");
+document.writeln("");
+document.writeln("<br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.writeln("208期团队八尾：01345678<br>");
+
+document.writeln("207期团队八尾：123567<span style='background-color: #FFFF00\'>8</span>9<br>");
+document.writeln("206期团队八尾：012<span style='background-color: #FFFF00\'>4</span>5789<br>");
+document.writeln("205期团队八尾：<span style='background-color: #FFFF00\'>0</span>1234789<br>");
+document.writeln("204期团队八尾：0123<span style='background-color: #FFFF00\'>5</span>679<br>");
+document.writeln("203期团队八尾：0134<span style='background-color: #FFFF00\'>5</span>678<br>");
+document.writeln("202期团队八尾：02<span style='background-color: #FFFF00\'>3</span>45689<br>");
+document.writeln("201期团队八尾：0134<span style='background-color: #FFFF00\'>5</span>679<br>");
+document.writeln("199期团队八尾：012<span style='background-color: #FFFF00\'>5</span>6789<br>");
+document.writeln("197期团队八尾：<span style='background-color: #FFFF00\'>0</span>1245689<br>");
+document.writeln("196期团队八尾：1245678<span style='background-color: #FFFF00\'>9</span><br>");
+document.writeln("195期团队八尾：0<span style='background-color: #FFFF00\'>1</span>345678<br>");
+document.writeln("194期团队八尾：123<span style='background-color: #FFFF00\'>5</span>6789<br>");
+document.writeln("193期团队八尾：012<span style='background-color: #FFFF00\'>4</span>5789<br>");
+document.writeln("192期团队八尾：0<span style='background-color: #FFFF00\'>1</span>234789<br>");
+document.writeln("191期团队八尾：0123567<span style='background-color: #FFFF00\'>9</span><br>");
+document.writeln("190期团队八尾：0<span style='background-color: #FFFF00\'>1</span>345678<br>");
+
+
+
+document.writeln("");
+document.writeln("");
+document.writeln("<br>");
+
+
+
+
+
+
+
+document.writeln("208期团队四头：0124<br>");
+document.writeln("207期团队四头：<span style='background-color: #FFFF00\'>1</span>234<br>");
+document.writeln("205期团队四头：013<span style='background-color: #FFFF00\'>4</span><br>");
+document.writeln("204期团队四头：<span style='background-color: #FFFF00\'>0</span>123<br>");
+document.writeln("203期团队四头：012<span style='background-color: #FFFF00\'>4</span><br>");
+document.writeln("202期团队四头：012<span style='background-color: #FFFF00\'>4</span><br>");
+document.writeln("200期团队四头：123<span style='background-color: #FFFF00\'>4</span><br>");
+document.writeln("199期团队四头：0<span style='background-color: #FFFF00\'>1</span>23<br>");
+document.writeln("198期团队四头：0<span style='background-color: #FFFF00\'>1</span>24<br>");
+document.writeln("197期团队四头：2<span style='background-color: #FFFF00\'>1</span>34<br>");
+document.writeln("196期团队四头：02<span style='background-color: #FFFF00\'>3</span>4<br>");
+document.writeln("195期团队四头：01<span style='background-color: #FFFF00\'>2</span>4<br>");
+document.writeln("193期团队四头：<span style='background-color: #FFFF00\'>0</span>234<br>");
+document.writeln("192期团队四头：01<span style='background-color: #FFFF00\'>3</span>4<br>");
+document.writeln("191期团队四头：012<span style='background-color: #FFFF00\'>3</span><br>");
+
+
+document.writeln("");
+document.writeln("");
+document.writeln("<br>");
+document.writeln("");
+
+
+
+document.writeln("208期团队四行：金水火土<br>");
+document.writeln("207期团队四行：金木火<span style='background-color: #FFFF00\'>土</span><br>");
+document.writeln("206期团队四行：<span style='background-color: #FFFF00\'>木</span>水火土<br>");
+document.writeln("205期团队四行：<span style='background-color: #FFFF00\'>金</span>木火土<br>");
+document.writeln("203期团队四行：金<span style='background-color: #FFFF00\'>木</span>火土<br>");
+document.writeln("202期团队四行：金<span style='background-color: #FFFF00\'>水</span>火土<br>");
+document.writeln("201期团队四行：<span style='background-color: #FFFF00\'>金</span>木火土<br>");
+document.writeln("200期团队四行：木<span style='background-color: #FFFF00\'>水</span>火土<br>");
+document.writeln("199期团队四行：金<span style='background-color: #FFFF00\'>木</span>水火<br>");
+document.writeln("198期团队四行：金木水<span style='background-color: #FFFF00\'>火</span><br>");
+document.writeln("197期团队四行：<span style='background-color: #FFFF00\'>金</span>木水土<br>");
+document.writeln("196期团队四行：金水<span style='background-color: #FFFF00\'>火</span>土<br>");
+document.writeln("195期团队四行：金<span style='background-color: #FFFF00\'>水</span>火土<br>");
+document.writeln("194期团队四行：金木火<span style='background-color: #FFFF00\'>土</span><br>");
+document.writeln("193期团队四行：木水火<span style='background-color: #FFFF00\'>土</span><br>");
+document.writeln("192期团队四行：金木<span style='background-color: #FFFF00\'>火</span>土<br>");
+document.writeln("191期团队四行：金木水<span style='background-color: #FFFF00\'>火</span><br>");
+document.writeln("190期团队四行：<span style='background-color: #FFFF00\'>金</span>木火土<br>");
+
+
+
+document.writeln("");
+document.writeln("<br>");
+document.writeln("");
+document.writeln("");
+document.writeln("    </font></b></font></table>");
+document.writeln("		");
+document.writeln("	<!--结束-->");
+document.writeln("  ");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("  ");
+document.writeln("");
+document.writeln("  ");
+document.writeln("");
+document.writeln(" ");
+document.writeln("");
+document.writeln("");
+document.writeln(" ");
+document.writeln("  ");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("<table style=border-collapse:collapse border=0 width=100%><tbody><tr><td height=40><p style=text-align:center><strong>");
+document.writeln("			<a target=\'_blank\' href=\'https://76891.com/#868627\' style=\'text-decoration: none\'><img src=/yyy123.gif style=\'width:20px\' height=\'19\'><span style=font-family:楷体;color:#00f><font style=\'font-size: 15pt\'>进入澳彩网</font></span><span style=font-family:楷体;font-size:17pt;><font color=\'#FF0000\'>76891.com</font></span></a></strong></tr></table>		");
+document.writeln(" ");
+document.writeln("");
+document.writeln("</table>");
+document.writeln("		</tbody>");
