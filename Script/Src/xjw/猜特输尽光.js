87e578f8f611c0f09b2eaec8def0741d013800b8
Rokate <77772464+Rokate@users.noreply.github.com>
2024-08-30 03:20:35 +0800

    Update:  2024.08.30 03:20:35

--- Script/Src/xjw/猜特输尽光.js
+++ Script/Src/xjw/猜特输尽光.js
@@ -12,12 +12,23 @@ document.writeln("	");
 document.writeln("			");
 
 
+document.writeln("<tr>");
+document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
+document.writeln("	");
+document.writeln("				<p>");
+document.writeln("	");
+document.writeln("		<span><font size=\'4\'>243期:<font color=\'#008000\'>【正版猜特输尽光】</font><font color=\'#FF0000\'>开？00</font><font color=\'#008000\'><br>");
+document.writeln("		</font><font color=\'#FF00FF\'>今期虎蛇输尽光，二头一尾全不买。</td>");
+document.writeln("		</tr>");
+document.writeln("		");
+document.writeln("");
+
 
 document.writeln("	<tr>");
 document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
 document.writeln("	");
-document.writeln("		<span><font size=\'4\'>242期:<font color=\'#008000\'>【正版猜特输尽光】</font><font color=\'#FF0000\'>开？00</font><font color=\'#008000\'><br>");
-document.writeln("		</font><font color=\'#FF00FF\'>今期兔鼠输尽光，零头六尾全不买。 </font></td>");
+document.writeln("		<span><font size=\'4\'>242期:<font color=\'#008000\'>【正版猜特输尽光】</font><font color=\'#FF0000\'>开猪30</font><font color=\'#008000\'><br>");
+document.writeln("		</font><font color=\'#FF00FF\'>今期<span style='background-color: #FFFF00\'>兔鼠</span>输尽光，<span style='background-color: #FFFF00\'>零头六尾</span>全不买。 </font></td>");
 document.writeln("		</tr>");
 document.writeln("");
 document.writeln("");
@@ -33,7 +44,7 @@ document.writeln("	");
 document.writeln("		<p>");
 document.writeln("	");
 document.writeln("		<span><font size=\'4\'>240期:<font color=\'#008000\'>【正版猜特输尽光】</font><font color=\'#FF0000\'>开蛇24</font><font color=\'#008000\'><br>");
-document.writeln("		</font><font color=\'#FF00FF\'>今期鼠猪输尽光，一头三尾全不买。</td>");
+document.writeln("		</font><font color=\'#FF00FF\'>今期<span style='background-color: #FFFF00\'>鼠猪</span>输尽光，<span style='background-color: #FFFF00\'>一头三尾</span>全不买。</td>");
 document.writeln("		</tr>");
 document.writeln("		");
 document.writeln("		");
