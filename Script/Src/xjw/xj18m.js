a9863a2d5d6b85c9102b427e29e038568d652e30
Rokate <77772464+Rokate@users.noreply.github.com>
2024-09-08 15:20:29 +0800

    Update:  2024.09.08 15:20:29

--- Script/Src/xjw/xj18m.js
+++ Script/Src/xjw/xj18m.js
@@ -29,7 +29,7 @@ document.writeln("</style>");
 document.writeln("");
 document.writeln("<body>");
 document.writeln("  ");
-document.writeln("<img src=\"https://tk2.baegg.com:4949/col/252/amzy.jpg\" alt=\"\" width=\"100%\">");
+document.writeln("<img src=\"https://tk2.wyvogue.com:4949/col/252/amzy.jpg\" alt=\"\" width=\"100%\">");
 document.writeln("");
 document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
 document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
