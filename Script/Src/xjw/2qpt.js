19783ef16d8df2f9fd7fcda59ac29730ea158afa
Rokate <77772464+Rokate@users.noreply.github.com>
2024-09-25 00:20:37 +0800

    Update:  2024.09.25 00:20:37

--- Script/Src/xjw/2qpt.js
+++ Script/Src/xjw/2qpt.js
@@ -50,7 +50,7 @@ document.writeln("		<td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' widt
 document.writeln("		<b><font face=\'微软雅黑\'>268-270期</font></b></td>");
 document.writeln("		<td align=\'center\' bgcolor=\'#FFFFFF\' width=\'65%\' rowspan=\'3\'>");
 document.writeln("		<strong>");
-document.writeln("		<font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'>蛇<br>（012.24.36）</font></strong></td>");
+document.writeln("		<font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'>蛇<br>（12.24.36）</font></strong></td>");
 document.writeln("		<td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' width=\'10%\'>");
 document.writeln("		<font face=\'微软雅黑\'>000期中</font></td>");
 document.writeln("		</tr>");
