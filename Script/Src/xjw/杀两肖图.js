2246719ac51ceb7213bd7965b5e7f45155c54f58
Rokate <77772464+Rokate@users.noreply.github.com>
2024-10-05 13:20:33 +0800

    Update:  2024.10.05 13:20:33

--- Script/Src/xjw/杀两肖图.js
+++ Script/Src/xjw/杀两肖图.js
@@ -35,8 +35,8 @@ document.writeln("	");
 document.writeln("			<tr style=\'box-sizing: border-box;\'>");
 document.writeln("			<td height=\'40\' style=\'box-sizing: border-box;\'>");
 document.writeln("			");
-document.writeln("<a target=\'_blank\' href=\'https://x1.xn--c2be8bbg0fl3ewb.xn--i1b6b1a6a2e:8443/#/picture/28585\'><img src=\'https://mtu.wyvogue.com:4949/col/278/c002.jpg\'  width=\'49%\'></a>");
-document.writeln("<a target=\'_blank\' href=\'https://x1.xn--c2be8bbg0fl3ewb.xn--i1b6b1a6a2e:8443/#/picture/209029\'><img src=\'https://mtu.wyvogue.com:4949/col/278/hj44.jpg\'  width=\'49%\'></a>");
+document.writeln("<a target=\'_blank\' href=\'https://x1.xn--c2be8bbg0fl3ewb.xn--i1b6b1a6a2e:8443/#/picture/28585\'><img src=\'https://mtu.wyvogue.com:4949/col/279/c002.jpg\'  width=\'49%\'></a>");
+document.writeln("<a target=\'_blank\' href=\'https://x1.xn--c2be8bbg0fl3ewb.xn--i1b6b1a6a2e:8443/#/picture/209029\'><img src=\'https://mtu.wyvogue.com:4949/col/279/hj44.jpg\'  width=\'49%\'></a>");
 document.writeln("		</td></tr>");
 document.writeln("		");
 document.writeln("		");
@@ -51,6 +51,21 @@ document.writeln("		");
 
 
 
+document.writeln("");
+document.writeln("");
+document.writeln("					<!--开始-->");
+document.writeln("				<tr style=\'box-sizing: border-box;\'>");
+document.writeln("					<td height=\'40\' style=\'box-sizing: border-box;\'>");
+document.writeln("			<p align=\'center\' style=\'box-sizing: border-box; padding: 0px; margin: 0px;\'>");
+document.writeln("			<font size=\'4\' style=\'margin: 0px; padding: 0px;\'>");
+document.writeln("			279期玄机网禁两肖<font color=\'#FF0000\'>【狗鸡】</font>特开<font color=\'#0000FF\'>？00</font>准<br>");
+document.writeln("			279期澳门杀两肖图<font color=\'#FF0000\'>【鼠狗】</font>特开<font color=\'#0000FF\'>？00</font>准<br>");
+document.writeln("			279期澳门图库禁肖<font color=\'#FF0000\'>【鼠马】</font>特开<font color=\'#0000FF\'>？00</font>准</font></font></p></td>");
+document.writeln("		</tr>");
+document.writeln("<!--结束-->	");
+document.writeln("		");
+
+
 
 
 document.writeln("<!--开始-->");
@@ -58,9 +73,9 @@ document.writeln("				<tr style=\'box-sizing: border-box;\'>");
 document.writeln("					<td height=\'40\' style=\'box-sizing: border-box;\'>");
 document.writeln("			<p align=\'center\' style=\'box-sizing: border-box; padding: 0px; margin: 0px;\'>");
 document.writeln("			<font size=\'4\' style=\'margin: 0px; padding: 0px;\'>");
-document.writeln("			278期玄机网禁两肖<font color=\'#FF0000\'>【猪牛】</font>特开<font color=\'#0000FF\'>？00</font>准<br>");
-document.writeln("			278期澳门杀两肖图<font color=\'#FF0000\'>【虎马】</font>特开<font color=\'#0000FF\'>？00</font>准<br>");
-document.writeln("			278期澳门图库禁肖<font color=\'#FF0000\'>【龙猴】</font>特开<font color=\'#0000FF\'>？00</font>准</font></font></p></td>");
+document.writeln("			278期玄机网禁两肖<font color=\'#FF0000\'>【猪牛】</font>特开<font color=\'#0000FF\'>狗07</font>准<br>");
+document.writeln("			278期澳门杀两肖图<font color=\'#FF0000\'>【虎马】</font>特开<font color=\'#0000FF\'>狗07</font>准<br>");
+document.writeln("			278期澳门图库禁肖<font color=\'#FF0000\'>【龙猴】</font>特开<font color=\'#0000FF\'>狗07</font>准</font></font></p></td>");
 document.writeln("		</tr>");
 document.writeln("<!--结束-->	");
 document.writeln("		");
