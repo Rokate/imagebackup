a7839f7ad753fe47e08caa86c8411dcc6db98868
Rokate <77772464+Rokate@users.noreply.github.com>
2024-11-21 13:20:34 +0800

    Update:  2024.11.21 13:20:34

--- Script/Src/xjw/zhsl.js
+++ Script/Src/xjw/zhsl.js
@@ -601,6 +601,20 @@ document.writeln("");
 document.writeln("");
 
 
+document.writeln("		<!--开始四特-->");
+document.writeln("           <li>");
+document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff height=\'37\'><tbody><tr>");
+document.writeln("	<td width=100% height=37><p align=center><font face=\'楷体\'><b><font size=\'4\'>");
+document.writeln("326期绝杀1.头<font color=\'#FF00FF\' size=\'5\'>【4头双】</font>开？00√</font></b></font></table>");
+document.writeln("</li>");
+document.writeln("		<!--结束-->");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+
+
 
 document.writeln("		<!--开始四特-->");
 document.writeln("           <li>");
