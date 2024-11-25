2240d86ff6614d26f72cf3cd10302338ac9f4b0a
Rokate <77772464+Rokate@users.noreply.github.com>
2024-11-26 00:20:36 +0800

    Update:  2024.11.26 00:20:36

--- Script/Src/xjw/pt2m.js
+++ Script/Src/xjw/pt2m.js
@@ -42,11 +42,20 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 
+document.writeln("<!--开始-->");
+document.writeln("			<tr>");
+document.writeln("			<td height=\'46\' bgcolor=\"#FFFFFF\">");
+document.writeln("					<p align=\'center\'><font size=\'4\' color=\'#000000\'>331期：独平</font><font color=\'#FF0000\' size=\'5\'>【牛28.16】</font><font color=\'#000000\' size=\'4\'>开00准</font><font color=\"#000000\"></b></font></td>");
+document.writeln("		</tr>");
+document.writeln("<!--结束-->");
+document.writeln("	");
+
+
 
 document.writeln("<!--开始-->");
 document.writeln("			<tr>");
 document.writeln("			<td height=\'46\' bgcolor=\"#FFFFFF\">");
-document.writeln("					<p align=\'center\'><font size=\'4\' color=\'#000000\'>330期：独平</font><font color=\'#FF0000\' size=\'5\'>【兔14.38】</font><font color=\'#000000\' size=\'4\'>开00准</font><font color=\"#000000\"></b></font></td>");
+document.writeln("					<p align=\'center\'><font size=\'4\' color=\'#000000\'>330期：独平</font><font color=\'#FF0000\' size=\'5\'>【<span style='background-color: #FFFF00\'>兔</span>14.38】</font><font color=\'#000000\' size=\'4\'>开02准</font><font color=\"#000000\"></b></font></td>");
 document.writeln("		</tr>");
 document.writeln("<!--结束-->");
 document.writeln("	");
