d23ded150ed466dec8b6bc6394ef54a446db6ce7
Rokate <77772464+Rokate@users.noreply.github.com>
2024-11-28 12:20:40 +0800

    Update:  2024.11.28 12:20:40

--- Script/Src/xjw/杀两肖图.js
+++ Script/Src/xjw/杀两肖图.js
@@ -35,7 +35,7 @@ document.writeln("	");
 document.writeln("			<tr style=\'box-sizing: border-box;\'>");
 document.writeln("			<td height=\'40\' style=\'box-sizing: border-box;\'>");
 document.writeln("			");
-document.writeln("<a target=\'_blank\' href=\'https://x1.xn--c2be8bbg0fl3ewb.xn--i1b6b1a6a2e:8443/#/picture/28585\'><img src=\'https://amo.ahhjzh.com:4949/col/332/c002.jpg\'  width=\'49%\'></a>");
+document.writeln("<a target=\'_blank\' href=\'https://x1.xn--c2be8bbg0fl3ewb.xn--i1b6b1a6a2e:8443/#/picture/28585\'><img src=\'https://amo.ahhjzh.com:4949/col/333/c002.jpg\'  width=\'49%\'></a>");
 document.writeln("<a target=\'_blank\' href=\'https://x1.xn--c2be8bbg0fl3ewb.xn--i1b6b1a6a2e:8443/#/picture/209029\'><img src=\'https://amo.ahhjzh.com:4949/col/333/hj44.jpg\'  width=\'49%\'></a>");
 document.writeln("		</td></tr>");
 document.writeln("		");
