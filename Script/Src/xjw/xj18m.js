ba6a1d3ea43e691531242afa69b82b1701521795
Rokate <77772464+Rokate@users.noreply.github.com>
2024-12-10 00:20:38 +0800

    Update:  2024.12.10 00:20:38

--- Script/Src/xjw/xj18m.js
+++ Script/Src/xjw/xj18m.js
@@ -29,7 +29,7 @@ document.writeln("</style>");
 document.writeln("");
 document.writeln("<body>");
 document.writeln("  ");
-document.writeln("<img src=\"https://amo.ahhjzh.com:4949/col/344/amzy.jpg\" alt=\"\" width=\"100%\">");
+document.writeln("<img src=\"https://amo.ahhjzh.com:4949/col/345/amzy.jpg\" alt=\"\" width=\"100%\">");
 document.writeln("");
 document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
 document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
@@ -48,6 +48,8 @@ document.writeln("");
 
 
 
+
+
 document.writeln("  <!--开始-->  ");
 document.writeln("    	<tr>");
 document.writeln("      <td height=38 >");
@@ -56,10 +58,31 @@ document.writeln("");
 document.writeln("		<font face=\'微软雅黑\'>");
 document.writeln("		<b>");
 document.writeln("");
-document.writeln("		<font color=\"#0000FF\">344期【玄机特码诗】猜透必中开（？00）</font><br>");
-document.writeln("①解特九肖：龙蛇猴鸡狗猪兔马羊<br>");
+document.writeln("		<font color=\"#0000FF\">345期【玄机特码诗】猜透必中开（？00）</font><br>");
+document.writeln("①解特九肖：牛马虎蛇猴狗羊鸡猪<br>");
+document.writeln("②解特五肖：牛马虎蛇猴<br>");
+document.writeln("③解特18码：40.28.11.47.15.39.12.48.09.33.31.43.22.46.08.44.06.30</b></font></p></td>");
+document.writeln("    </tr>     ");
+document.writeln("     <!--结束-->");
+document.writeln("	");
+document.writeln("	");
+document.writeln("");
+
+
+
+
+document.writeln("  <!--开始-->  ");
+document.writeln("    	<tr>");
+document.writeln("      <td height=38 >");
+document.writeln("		<p align=\'left\'>");
+document.writeln("");
+document.writeln("		<font face=\'微软雅黑\'>");
+document.writeln("		<b>");
+document.writeln("");
+document.writeln("		<font color=\"#0000FF\">344期【玄机特码诗】猜透必中开（兔02）</font><br>");
+document.writeln("①解特九肖：龙蛇猴鸡狗猪<span style='background-color: #FFFF00\'>兔</span>马羊<br>");
 document.writeln("②解特五肖：龙蛇猴鸡狗<br>");
-document.writeln("③解特18码：25.37.12.48.09.33.08.44.19.43.06.42.14.02.23.47.22.46</b></font></p></td>");
+document.writeln("③解特18码：25.37.12.48.09.33.08.44.19.43.06.42.14.<span style='background-color: #FFFF00\'>02</span>.23.47.22.46</b></font></p></td>");
 document.writeln("    </tr>     ");
 document.writeln("     <!--结束-->");
 document.writeln("	");
