47129ab52745321ad4ae282ca4c3789d8ec31300
Rokate <77772464+Rokate@users.noreply.github.com>
2024-12-14 00:20:41 +0800

    Update:  2024.12.14 00:20:41

---
+++ Script/Src/xjw/三肖三码.js
@@ -0,0 +1,47 @@
+﻿
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
+document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
+document.writeln("<b><font face=\'楷体\' style=\'font-size: 20pt\'><font color=\'#FFFF00\'>澳门玄机86563</font><font color=\'#FFFFFF\'>『三肖三码』</font></font></b></table>");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("<table border=\'1\' width=\'100%\' cellpadding=\'0\' cellspacing=\'0\' bordercolorlight=\'#FFFFFF\' bordercolordark=\'#FFFFFF\' bgcolor=\'#FFFFFF\'>");
+document.writeln("		");
+document.writeln("		");
+document.writeln("		");
+
+
+	
+
+document.writeln("");
+document.writeln("");
+document.writeln("	");
+document.writeln("");
+document.writeln("						<tr>");
+document.writeln("		<td class=\'center f11 black l150\'>");
+document.writeln("		<p align=\'center\'>");
+document.writeln("	");
+document.writeln("		<span style=\'vertical-align: middle\'><font size=\'4\'>349期:<font color=\'#FF00FF\'>《马羊鸡》</font><font color=\'#008000\'>（35 46 08）</font><font color=\'#0000FF\'>开？00中</font></font></span></div>	");
+document.writeln("	");
+document.writeln("");
+document.writeln("	");
+
+
+
+
+document.writeln("");
+document.writeln("");
+document.writeln("						<tr>");
+document.writeln("		<td class=\'center f11 black l150\'>");
+document.writeln("");
+document.writeln("			<p align=\'center\'>");
+document.writeln("	");
+document.writeln("		<span style=\'vertical-align: middle\'><font size=\'4\'>347期:<font color=\'#FF00FF\'>《兔<span style='background-color: #FFFF00\'>牛</span>鸡》</font><font color=\'#008000\'>（38 16 08）</font><font color=\'#0000FF\'>开牛28中</font></font></span></div>	");
+document.writeln("		");
+document.writeln("");
+
+
+
