a00e89b76a5c26bd3af117063b48a627bda15e55
Rokate2025 <77772464+Rokate@users.noreply.github.com>
2025-01-02 15:06:28 +0800

    Update Download.py

--- Script/Download.py
+++ Script/Download.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
         info = requests.get('https://h5.118z2.com:8443/tk118/h5/picture/detail/latest?pictureTypeId=28854').json()
         #qish = requests.post('https://am49.app/open/latest/single?code=71').json()['data']['nextIssueNo'][-3:]
         qish = info['data']['period']
-        qishu = '00'+ qish
+        qishu = '00'+ str(qish)
         #qishu = qish.lstrip('0')
         #Lday = requests.get('https://49252a.com/unite49/h5/index/lotteryTime')
         #qishu = Lday.json()['data']['list'][0]['isLotteryDay'] + 1
