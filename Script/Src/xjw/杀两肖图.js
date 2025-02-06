27d4b842accfc0b46b6ecdd2e1cbbdc91e0cf12c
Rokate <77772464+Rokate@users.noreply.github.com>
2025-02-07 04:20:34 +0800

    Update:  2025.02.07 04:20:34

--- Script/Src/xjw/杀两肖图.js
+++ Script/Src/xjw/杀两肖图.js
@@ -56,24 +56,18 @@ document.writeln("		");
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.writeln("	");
+document.writeln("									<!--开始-->");
+document.writeln("				<tr style=\'box-sizing: border-box;\'>");
+document.writeln("					<td height=\'40\' style=\'box-sizing: border-box;\'>");
+document.writeln("			<p align=\'center\' style=\'box-sizing: border-box; padding: 0px; margin: 0px;\'>");
+document.writeln("			<font size=\'4\' style=\'margin: 0px; padding: 0px;\'>");
+document.writeln("			038期玄机网禁两肖<font color=\'#FF0000\'>【虎羊】</font>特开<font color=\'#0000FF\'>蛇25</font>准<br>");
+document.writeln("			038期澳门杀两肖图<font color=\'#FF0000\'>【马鸡】</font>特开<font color=\'#0000FF\'>蛇25</font>准<br>");
+document.writeln("			038期澳门图库禁肖<font color=\'#FF0000\'>【鸡马】</font>特开<font color=\'#0000FF\'>蛇25</font>准</font></font></p></td>");
+document.writeln("		</tr>");
+document.writeln("<!--结束-->	");
+document.writeln("		");
 
 
 
