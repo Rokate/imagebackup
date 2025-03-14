cc2e30bd25e913864ca3f4390845c9363c51341e
Rokate <77772464+Rokate@users.noreply.github.com>
2025-03-14 09:20:36 +0800

    Update:  2025.03.14 09:20:36

--- Script/Src/xjw/xj18m.js
+++ Script/Src/xjw/xj18m.js
@@ -33,7 +33,7 @@ document.writeln("<img src=\"https://amo.phseo1.com:4949/col/73/amzy.jpg\" alt=\
 document.writeln("");
 document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
 document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
-document.writeln("<b><font face=\'楷体\' style=\'font-size: 18pt\'><font color=\'#FFFF00\'>澳门玄机86563</font><font color=\'#FFFFFF\'>『玄机18码』</font></font></b></table>");
+document.writeln("<b><font face=\'楷体\' style=\'font-size: 18pt\'><font color=\'#FFFF00\'>澳门玄机86281</font><font color=\'#FFFFFF\'>『玄机18码』</font></font></b></table>");
 document.writeln("");
 document.writeln("");
 document.writeln("  ");
@@ -91,28 +91,6 @@ document.writeln("	");
 document.writeln("	");
 document.writeln("");
 
-
-
-
-
-
-document.writeln("  <!--开始-->  ");
-document.writeln("    	<tr>");
-document.writeln("      <td height=38 >");
-document.writeln("		<p align=\'left\'>");
-document.writeln("");
-document.writeln("		<font face=\'微软雅黑\'>");
-document.writeln("		<b>");
-document.writeln("");
-document.writeln("		<font color=\"#0000FF\">069期【玄机特码诗】猜透必中开（牛29）</font><br>");
-document.writeln("①解特九肖：羊兔龙狗马猪<span style='background-color: #FFFF00\'>牛</span>鸡虎<br>");
-document.writeln("②解特五肖：羊兔龙狗马<br>");
-document.writeln("③解特18码：11.23.03.15.14.26.20.32.12.36.31.43.05.17.21.33.04.16</b></font></p></td>");
-document.writeln("    </tr>     ");
-document.writeln("     <!--结束-->");
-document.writeln("	");
-document.writeln("	");
-document.writeln("");
 
 
 
