632b0229e1f6155df30fcde0010c0ccba3346112
Rokate <77772464+Rokate@users.noreply.github.com>
2025-04-16 23:20:43 +0800

    Update:  2025.04.16 23:20:43

--- Script/Src/xjw/xj18m.js
+++ Script/Src/xjw/xj18m.js
@@ -29,7 +29,7 @@ document.writeln("</style>");
 document.writeln("");
 document.writeln("<body>");
 document.writeln("  ");
-document.writeln("<img src=\"https://amo.yaoan-learn.com:4949/col/106/amzy.jpg\" alt=\"\" width=\"100%\">");
+document.writeln("<img src=\"https://amo.yaoan-learn.com:4949/col/107/amzy.jpg\" alt=\"\" width=\"100%\">");
 document.writeln("");
 document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
 document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
@@ -48,25 +48,6 @@ document.writeln("");
 
 
 
-document.writeln("  <!--开始-->  ");
-document.writeln("    	<tr>");
-document.writeln("      <td height=38 >");
-document.writeln("		<p align=\'left\'>");
-document.writeln("");
-document.writeln("		<font face=\'微软雅黑\'>");
-document.writeln("		<b>");
-document.writeln("");
-document.writeln("		<font color=\"#0000FF\">106期【玄机特码诗】猜透必中开（？00）</font><br>");
-document.writeln("①解特九肖：龙蛇猴鸡狗猪兔马羊<br>");
-document.writeln("②解特五肖：龙蛇猴鸡狗<br>");
-document.writeln("③解特18码：26.38.13.49.10.34.09.45.20.44.07.43.15.03.24.48.23.47</b></font></p></td>");
-document.writeln("    </tr>     ");
-document.writeln("     <!--结束-->");
-document.writeln("	");
-document.writeln("	");
-
-
-
 
 
 document.writeln("  <!--开始-->  ");
@@ -77,122 +58,15 @@ document.writeln("");
 document.writeln("		<font face=\'微软雅黑\'>");
 document.writeln("		<b>");
 document.writeln("");
-document.writeln("		<font color=\"#0000FF\">104期【玄机特码诗】猜透必中开（鼠06）</font><br>");
-document.writeln("①解特九肖：虎猴牛兔<span style='background-color: #FFFF00\'>鼠</span>猪羊鸡狗<br>");
-document.writeln("②解特五肖：虎猴牛兔<span style='background-color: #FFFF00\'>鼠</span><br>");
-document.writeln("③解特18码：04.40.10.34.05.29.15.39.18.30.07.19.35.47.09.21.20.32</b></font></p></td>");
-document.writeln("    </tr>     ");
-document.writeln("     <!--结束-->");
-document.writeln("	");
-document.writeln("	");
-
-
-
-
-document.writeln("  <!--开始-->  ");
-document.writeln("    	<tr>");
-document.writeln("      <td height=38 >");
-document.writeln("		<p align=\'left\'>");
-document.writeln("");
-document.writeln("		<font face=\'微软雅黑\'>");
-document.writeln("		<b>");
-document.writeln("");
-document.writeln("		<font color=\"#0000FF\">103期【玄机特码诗】猜透必中开（狗32）</font><br>");
-document.writeln("①解特九肖：蛇马兔龙猴鼠鸡<span style='background-color: #FFFF00\'>狗</span>猪<br>");
-document.writeln("②解特五肖：蛇马兔龙猴<br>");
-document.writeln("③解特18码：13.49.48.36.27.39.14.26.10.34.18.30.09.33.08.<span style='background-color: #FFFF00\'>32</span>.07.19</b></font></p></td>");
-document.writeln("    </tr>     ");
-document.writeln("     <!--结束-->");
-document.writeln("	");
-document.writeln("	");
-
-
-
-document.writeln("  <!--开始-->  ");
-document.writeln("    	<tr>");
-document.writeln("      <td height=38 >");
-document.writeln("		<p align=\'left\'>");
-document.writeln("");
-document.writeln("		<font face=\'微软雅黑\'>");
-document.writeln("		<b>");
-document.writeln("");
-document.writeln("		<font color=\"#0000FF\">102期【玄机特码诗】猜透必中开（龙38）</font><br>");
-document.writeln("①解特九肖：马牛<span style='background-color: #FFFF00\'>龙</span>猴兔鼠猪羊虎<br>");
-document.writeln("②解特五肖：马牛<span style='background-color: #FFFF00\'>龙</span>猴兔<br>");
-document.writeln("③解特18码：24.36.29.41.14.26.10.22.27.39.18.30.19.31.11.23.16.28</b></font></p></td>");
-document.writeln("    </tr>     ");
-document.writeln("     <!--结束-->");
-
-
-
-
-
-
-
-
-
-document.writeln("  <!--开始-->  ");
-document.writeln("    	<tr>");
-document.writeln("      <td height=38 >");
-document.writeln("		<p align=\'left\'>");
-document.writeln("");
-document.writeln("		<font face=\'微软雅黑\'>");
-document.writeln("		<b>");
-document.writeln("");
-document.writeln("		<font color=\"#0000FF\">100期【玄机特码诗】猜透必中开（鼠42）</font><br>");
-document.writeln("①解特九肖：虎兔羊鸡猪<span style='background-color: #FFFF00\'>鼠</span>马猴狗<br>");
-document.writeln("②解特五肖：虎兔羊鸡猪<br>");
-document.writeln("③解特18码：16.28.27.39.35.47.45.21.43.31.<span style='background-color: #FFFF00\'>42</span>.18.48.36.34.46.08.20</b></font></p></td>");
+document.writeln("		<font color=\"#0000FF\">107期【玄机特码诗】猜透必中开（？00）</font><br>");
+document.writeln("①解特九肖：虎羊兔马鸡猪猴狗鼠<br>");
+document.writeln("②解特五肖：虎羊兔马鸡<br>");
+document.writeln("③解特18码：40.28.11.47.15.39.12.48.09.33.31.43.22.46.08.44.06.30</b></font></p></td>");
 document.writeln("    </tr>     ");
 document.writeln("     <!--结束-->");
 document.writeln("	");
 document.writeln("	");
 document.writeln("");
-
-
-
-
-
-document.writeln("  <!--开始-->  ");
-document.writeln("    	<tr>");
-document.writeln("      <td height=38 >");
-document.writeln("		<p align=\'left\'>");
-document.writeln("");
-document.writeln("		<font face=\'微软雅黑\'>");
-document.writeln("		<b>");
-document.writeln("");
-document.writeln("		<font color=\"#0000FF\">099期【玄机特码诗】猜透必中开（狗20）</font><br>");
-document.writeln("①解特九肖：龙蛇马羊猴鸡<span style='background-color: #FFFF00\'>狗</span>猪鼠<br>");
-document.writeln("②解特五肖：龙蛇马羊猴<br>");
-document.writeln("③解特18码：26.38.25.37.12.24.35.47.34.46.09.33.08.32.19.31.06.30</b></font></p></td>");
-document.writeln("    </tr>     ");
-document.writeln("     <!--结束-->");
-document.writeln("	");
-document.writeln("");
-
-
-
-
-document.writeln("  <!--开始-->  ");
-document.writeln("    	<tr>");
-document.writeln("      <td height=38 >");
-document.writeln("		<p align=\'left\'>");
-document.writeln("");
-document.writeln("		<font face=\'微软雅黑\'>");
-document.writeln("		<b>");
-document.writeln("");
-document.writeln("		<font color=\"#0000FF\">098期【玄机特码诗】猜透必中开（兔03）</font><br>");
-document.writeln("①解特九肖：牛虎<span style='background-color: #FFFF00\'>兔</span>龙蛇马狗猪鼠<br>");
-document.writeln("②解特五肖：牛虎<span style='background-color: #FFFF00\'>兔</span>龙蛇<br>");
-document.writeln("③解特18码：29.17.40.28.<span style='background-color: #FFFF00\'>03</span>.39.14.26.25.37.12.24.20.32.07.31.30.18</b></font></p></td>");
-document.writeln("    </tr>     ");
-document.writeln("     <!--结束-->");
-document.writeln("	");
-document.writeln("	");
-document.writeln("");
-
-
-
 
 
 
