53f04cce00f8ed146700a16203fe748565d586fa
Rokate <77772464+Rokate@users.noreply.github.com>
2025-05-14 23:20:37 +0800

    Update:  2025.05.14 23:20:37

--- Script/Src/xjw/wh1b.js
+++ Script/Src/xjw/wh1b.js
@@ -52,8 +52,21 @@ document.writeln("	<!--gavc-->										");
 document.writeln("	<tr>");
 document.writeln("		<td bgcolor=\"#FFFFFF\" height=\"38\" align=\"center\"> <b>");
 document.writeln("		");
-document.writeln("<span style=\"font-size: 16pt\">134期 网红一波<font color=\"#FF0000\">【蓝波】</font><BR>");
-document.writeln("网红双波<font color=\"#FF0000\">【蓝波绿波】</font>开？00</span></td>");
+document.writeln("<span style=\"font-size: 16pt\">135期 网红一波<font color=\"#FF0000\">【绿波】</font><BR>");
+document.writeln("网红双波<font color=\"#FF0000\">【绿波蓝波】</font>开？00</span></td>");
+document.writeln("	</tr>");
+document.writeln("	<!--结束-->	");
+document.writeln("");
+
+
+
+
+document.writeln("	<!--gavc-->										");
+document.writeln("	<tr>");
+document.writeln("		<td bgcolor=\"#FFFFFF\" height=\"38\" align=\"center\"> <b>");
+document.writeln("		");
+document.writeln("<span style=\"font-size: 16pt\">134期 网红一波<font color=\"#FF0000\">【<span style='background-color: #FFFF00\'>蓝波</span>】</font><BR>");
+document.writeln("网红双波<font color=\"#FF0000\">【<span style='background-color: #FFFF00\'>蓝波</span>绿波】</font>开龙14</span></td>");
 document.writeln("	</tr>");
 document.writeln("	<!--结束-->	");
 document.writeln("");
--- Script/Src/xjw/xj18m.js
+++ Script/Src/xjw/xj18m.js
@@ -29,7 +29,7 @@ document.writeln("</style>");
 document.writeln("");
 document.writeln("<body>");
 document.writeln("  ");
-document.writeln("<img src=\"https://amo.yaoan-learn.com:4949/col/134/amzy.jpg\" alt=\"\" width=\"100%\">");
+document.writeln("<img src=\"https://amo.yaoan-learn.com:4949/col/135/amzy.jpg\" alt=\"\" width=\"100%\">");
 document.writeln("");
 document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
 document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
@@ -57,19 +57,14 @@ document.writeln("");
 document.writeln("		<font face=\'微软雅黑\'>");
 document.writeln("		<b>");
 document.writeln("");
-document.writeln("		<font color=\"#0000FF\">134期【玄机特码诗】猜透必中开（？00）</font><br>");
-document.writeln("①解特九肖：虎猴牛兔鼠猪羊鸡狗<br>");
-document.writeln("②解特五肖：虎猴牛兔鼠<br>");
-document.writeln("③解特18码：04.40.10.34.05.29.15.39.18.30.07.19.35.47.09.21.20.32</b></font></p></td>");
+document.writeln("		<font color=\"#0000FF\">135期【玄机特码诗】猜透必中开（？00）</font><br>");
+document.writeln("①解特九肖：鼠牛虎鸡狗猪马羊猴<br>");
+document.writeln("②解特五肖：鼠牛虎鸡狗<br>");
+document.writeln("③解特18码：06.18.41.17.04.40.45.33.44.32.43.19.36.48.35.47.10.34</b></font></p></td>");
 document.writeln("    </tr>     ");
 document.writeln("     <!--结束-->");
 document.writeln("	");
-document.writeln("	");
-
-
-
-
-
+document.writeln("");
 
 
 
