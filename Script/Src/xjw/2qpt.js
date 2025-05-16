ff5177ebe28890e5f5d8a809c09c3d3aa24bd86c
Rokate <77772464+Rokate@users.noreply.github.com>
2025-05-16 13:20:34 +0800

    Update:  2025.05.16 13:20:34

--- Script/Src/xjw/2qpt.js
+++ Script/Src/xjw/2qpt.js
@@ -53,10 +53,6 @@ document.writeln("");
 
 
 
-
-
-
-
 document.writeln("	<!----开始---->    ");
 document.writeln("	<table border=\'1\' width=\'100%\' cellpadding=\'0\' height=\'83\' cellspacing=\'0\' bgcolor=\'#FFFFFF\' bordercolor=\'#D4D4D4\' style=\'border-collapse: collapse\'>");
 document.writeln("		<tr>");
@@ -64,15 +60,16 @@ document.writeln("		<td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' widt
 document.writeln("		<b><font face=\'微软雅黑\'>134-136期</font></b></td>");
 document.writeln("		<td align=\'center\' bgcolor=\'#FFFFFF\' width=\'65%\' rowspan=\'3\'>");
 document.writeln("		<strong>");
-document.writeln("		<font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'>牛（05.17.29）</font></strong></td>");
+document.writeln("		<font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'>鸡（<span style='background-color: #FFFF00\'>09.21.45）</font></strong></td>");
 document.writeln("		<td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' width=\'10%\'>");
-document.writeln("		<font face=\'微软雅黑\'>000中</font></td>");
+document.writeln("		<font face=\'微软雅黑\'>0000中</font></td>");
 document.writeln("		</tr>");
 document.writeln("");
 document.writeln("");
 document.writeln("		</table>");
 document.writeln("		");
 document.writeln("");
+
 
 
 
