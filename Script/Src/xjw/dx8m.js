2326e1f5bf8ae3f1aebd27416536be32b08ae37e
Rokate <77772464+Rokate@users.noreply.github.com>
2025-06-06 10:20:33 +0800

    Update:  2025.06.06 10:20:33

--- Script/Src/xjw/dx8m.js
+++ Script/Src/xjw/dx8m.js
@@ -317,4 +317,69 @@ document.writeln("<p align=\"center\"><font size=\"4\">");
 document.writeln("132期玄机出版<font color=\"#0000FF\">【大小+8码】</font>开龙02准</font><br>");
 document.writeln("<font color=\"#FF00FF\"><span style=\"font-size: 16pt\">");
 document.writeln("<span style='background-color: #FFFF00\'>小数</span>+26.28.30.33.35.38.42.46</span></font></td>");
-document.writeln("</tr>	
+document.writeln("</tr>			");
+document.writeln("<!----结束----> ");
+document.writeln("	");
+
+
+
+
+document.writeln("	<!----开始---->    ");
+document.writeln("<tr>");
+document.writeln("<td height=40 class=\'stylelxz\' align=\"center\">");
+document.writeln("<p align=\"center\"><font size=\"4\">");
+document.writeln("131期玄机出版<font color=\"#0000FF\">【大小+8码】</font>开羊47准</font><br>");
+document.writeln("<font color=\"#FF00FF\"><span style=\"font-size: 16pt\">");
+document.writeln("<span style='background-color: #FFFF00\'>大数</span>+01.02.03.04.11.22.15.17</span></font></td>");
+document.writeln("</tr>			");
+document.writeln("<!----结束----> ");
+document.writeln("	");
+
+
+
+
+
+
+
+
+document.writeln("	<!----开始---->    ");
+document.writeln("<tr>");
+document.writeln("<td height=40 class=\'stylelxz\' align=\"center\">");
+document.writeln("<p align=\"center\"><font size=\"4\">");
+document.writeln("130期玄机出版<font color=\"#0000FF\">【大小+8码】</font>开羊23准</font><br>");
+document.writeln("<font color=\"#FF00FF\"><span style=\"font-size: 16pt\">");
+document.writeln("<span style='background-color: #FFFF00\'>小数</span>+31.42.34.26.28.29.41.45</span></font></td>");
+document.writeln("</tr>			");
+document.writeln("<!----结束----> ");
+document.writeln("	");
+
+
+
+
+document.writeln("	<!----开始---->    ");
+document.writeln("<tr>");
+document.writeln("<td height=40 class=\'stylelxz\' align=\"center\">");
+document.writeln("<p align=\"center\"><font size=\"4\">");
+document.writeln("129期玄机出版<font color=\"#0000FF\">【大小+8码】</font>开虎40准</font><br>");
+document.writeln("<font color=\"#FF00FF\"><span style=\"font-size: 16pt\">");
+document.writeln("<span style='background-color: #FFFF00\'>大数</span>+21.23.05.07.11.13.15.17</span></font></td>");
+document.writeln("</tr>			");
+document.writeln("<!----结束----> ");
+document.writeln("	");
+
+
+
+
+
+
+
+
+
+
+document.writeln("	");
+document.writeln("	");
+document.writeln("");
+document.writeln("	");
+document.writeln("");
+document.writeln("			</table>	");
+document.writeln("</body>");
