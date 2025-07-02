94995c9c4cc307786e368e988cb924c68479b1ab
Rokate <77772464+Rokate@users.noreply.github.com>
2025-07-03 00:20:37 +0800

    Update:  2025.07.03 00:20:37

--- Script/Src/xjw/2qpt.js
+++ Script/Src/xjw/2qpt.js
@@ -45,6 +45,23 @@ document.writeln("");
 
 
 
+document.writeln("    <!----开始---->    ");
+document.writeln("    <table border=\'1\' width=\'100%\' cellpadding=\'0\' height=\'83\' cellspacing=\'0\' bgcolor=\'#FFFFFF\' bordercolor=\'#D4D4D4\' style=\'border-collapse: collapse\'>");
+document.writeln("        <tr>");
+document.writeln("        <td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' width=\'10%\'>");
+document.writeln("        <b><font face=\'微软雅黑\'>184-186期</font></b></td>");
+document.writeln("        <td align=\'center\' bgcolor=\'#FFFFFF\' width=\'65%\' rowspan=\'3\'>");
+document.writeln("        <strong>");
+document.writeln("        <font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'>鸡（09.33.45）</font></strong></td>");
+document.writeln("        <td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' width=\'10%\'>");
+document.writeln("        <font face=\'微软雅黑\'>000中</font></td>");
+document.writeln("        </tr>");
+document.writeln("");
+document.writeln("");
+document.writeln("        </table>");
+document.writeln("        ");
+document.writeln("");
+
 
 document.writeln("    <!----开始---->    ");
 document.writeln("    <table border=\'1\' width=\'100%\' cellpadding=\'0\' height=\'83\' cellspacing=\'0\' bgcolor=\'#FFFFFF\' bordercolor=\'#D4D4D4\' style=\'border-collapse: collapse\'>");
@@ -53,9 +70,9 @@ document.writeln("        <td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\
 document.writeln("        <b><font face=\'微软雅黑\'>183-185期</font></b></td>");
 document.writeln("        <td align=\'center\' bgcolor=\'#FFFFFF\' width=\'65%\' rowspan=\'3\'>");
 document.writeln("        <strong>");
-document.writeln("        <font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'>马（12.36.48）</font></strong></td>");
+document.writeln("        <font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'>马（<span style='background-color: #FFFF00\'>12</span>.36.48）</font></strong></td>");
 document.writeln("        <td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' width=\'10%\'>");
-document.writeln("        <font face=\'微软雅黑\'>000中</font></td>");
+document.writeln("        <font face=\'微软雅黑\'>183中</font></td>");
 document.writeln("        </tr>");
 document.writeln("");
 document.writeln("");
