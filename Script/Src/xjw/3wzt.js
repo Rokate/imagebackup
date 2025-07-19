af7852ec6cd193bb5ebfac89b5fd88d05a137cc0
Rokate <77772464+Rokate@users.noreply.github.com>
2025-07-19 23:20:35 +0800

    Update:  2025.07.19 23:20:35

--- Script/Src/xjw/3wzt.js
+++ Script/Src/xjw/3wzt.js
@@ -43,12 +43,24 @@ document.writeln("");
 document.writeln("");
 
 
+
 document.writeln("	<!----开始---->    ");
 document.writeln("<tr>");
 document.writeln("<td height=40 class=\'stylelxz\' align=\"center\">");
 document.writeln("<p align=\"center\"><font size=\"4\">");
-document.writeln("200期六尾</font><font color=\"#FF0000\" size=\"5\">（6-9-7-2-0-8）</font><font size=\"4\"><br>");
-document.writeln("200期三尾</font><font color=\"#FF0000\" size=\"5\">（6-9-7）</font>");
+document.writeln("201期六尾</font><font color=\"#FF0000\" size=\"5\">（8-6-0-3-1-5）</font><font size=\"4\"><br>");
+document.writeln("201期三尾</font><font color=\"#FF0000\" size=\"5\">（8-6-0）</font>");
+document.writeln("</td>");
+document.writeln("</tr>			");
+document.writeln("<!----结束----> ");
+document.writeln("");
+
+document.writeln("	<!----开始---->    ");
+document.writeln("<tr>");
+document.writeln("<td height=40 class=\'stylelxz\' align=\"center\">");
+document.writeln("<p align=\"center\"><font size=\"4\">");
+document.writeln("200期六尾</font><font color=\"#FF0000\" size=\"5\">（6-9-<span style='background-color: #FFFF00\'>7</span>-2-0-8）</font><font size=\"4\"><br>");
+document.writeln("200期三尾</font><font color=\"#FF0000\" size=\"5\">（6-9-<span style='background-color: #FFFF00\'>7</span>）</font>");
 document.writeln("</td>");
 document.writeln("</tr>			");
 document.writeln("<!----结束----> ");
--- Script/Src/xjw/xj18m.js
+++ Script/Src/xjw/xj18m.js
@@ -29,7 +29,7 @@ document.writeln("</style>");
 document.writeln("");
 document.writeln("<body>");
 document.writeln("  ");
-document.writeln("<img src=\"https://amo.sanyiyuntong.com:4949/col/200/amzy.jpg\" alt=\"\" width=\"100%\">");
+document.writeln("<img src=\"https://amo.sanyiyuntong.com:4949/col/201/amzy.jpg\" alt=\"\" width=\"100%\">");
 document.writeln("");
 document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
 document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
@@ -48,6 +48,7 @@ document.writeln("");
 
 
 
+
 document.writeln("  <!--开始-->  ");
 document.writeln("    	<tr>");
 document.writeln("      <td height=38 >");
@@ -56,10 +57,29 @@ document.writeln("");
 document.writeln("		<font face=\'微软雅黑\'>");
 document.writeln("		<b>");
 document.writeln("");
-document.writeln("		<font color=\"#0000FF\">200期【玄机特码诗】猜透必中开（？00）</font><br>");
-document.writeln("①解特九肖：龙猴蛇鸡羊狗兔鼠猪<br>");
-document.writeln("②解特五肖：龙猴蛇鸡羊<br>");
-document.writeln("③解特18码：14.26.10.22.25.27.33.46.35.47.20.32.15.27.18.30.07.19</b></font></p></td>");
+document.writeln("		<font color=\"#0000FF\">201期【玄机特码诗】猜透必中开（？00）</font><br>");
+document.writeln("①解特九肖：虎猴牛鸡鼠猪马蛇龙<br>");
+document.writeln("②解特五肖：虎猴牛鸡鼠<br>");
+document.writeln("③解特18码：04.16.34.46.17.29.09.33.06.30.19.31.12.48.01.13.14.26</b></font></p></td>");
+document.writeln("    </tr>     ");
+document.writeln("     <!--结束-->");
+document.writeln("	");
+document.writeln("	");
+
+
+
+document.writeln("  <!--开始-->  ");
+document.writeln("    	<tr>");
+document.writeln("      <td height=38 >");
+document.writeln("		<p align=\'left\'>");
+document.writeln("");
+document.writeln("		<font face=\'微软雅黑\'>");
+document.writeln("		<b>");
+document.writeln("");
+document.writeln("		<font color=\"#0000FF\">200期【玄机特码诗】猜透必中开（羊47）</font><br>");
+document.writeln("①解特九肖：龙猴蛇鸡<span style='background-color: #FFFF00\'>羊</span>狗兔鼠猪<br>");
+document.writeln("②解特五肖：龙猴蛇鸡<span style='background-color: #FFFF00\'>羊</span><br>");
+document.writeln("③解特18码：14.26.10.22.25.27.33.46.35.<span style='background-color: #FFFF00\'>47</span>.20.32.15.27.18.30.07.19</b></font></p></td>");
 document.writeln("    </tr>     ");
 document.writeln("     <!--结束-->");
 document.writeln("	");
