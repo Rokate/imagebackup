fc181a022db07d02866bc9c47adf642984aa9b02
Rokate <77772464+Rokate@users.noreply.github.com>
2025-08-14 00:20:33 +0800

    Update:  2025.08.14 00:20:33

--- Script/Src/xjw/xj18m.js
+++ Script/Src/xjw/xj18m.js
@@ -29,7 +29,7 @@ document.writeln("</style>");
 document.writeln("");
 document.writeln("<body>");
 document.writeln("  ");
-document.writeln("<img src=\"https://amo.sanyiyuntong.com:4949/col/225/amzy.jpg\" alt=\"\" width=\"100%\">");
+document.writeln("<img src=\"https://amo.sanyiyuntong.com:4949/col/226/amzy.jpg\" alt=\"\" width=\"100%\">");
 document.writeln("");
 document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
 document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
@@ -58,8 +58,27 @@ document.writeln("");
 document.writeln("		<font face=\'微软雅黑\'>");
 document.writeln("		<b>");
 document.writeln("");
-document.writeln("		<font color=\"#0000FF\">225期【玄机特码诗】猜透必中开（？00）</font><br>");
-document.writeln("①解特九肖：猴龙羊马虎狗牛蛇鸡<br>");
+document.writeln("		<font color=\"#0000FF\">226期【玄机特码诗】猜透必中开（？00）</font><br>");
+document.writeln("①解特九肖：虎鼠鸡兔狗蛇马牛猴<br>");
+document.writeln("②解特五肖：虎鼠鸡兔狗<br>");
+document.writeln("③解特18码：28.40.30.42.21.45.15.27.20.32.01.13.12.24.19.29.10.22</b></font></p></td>");
+document.writeln("    </tr>     ");
+document.writeln("     <!--结束-->");
+document.writeln("	");
+document.writeln("	");
+
+
+
+document.writeln("  <!--开始-->  ");
+document.writeln("    	<tr>");
+document.writeln("      <td height=38 >");
+document.writeln("		<p align=\'left\'>");
+document.writeln("");
+document.writeln("		<font face=\'微软雅黑\'>");
+document.writeln("		<b>");
+document.writeln("");
+document.writeln("		<font color=\"#0000FF\">225期【玄机特码诗】猜透必中开（狗32）</font><br>");
+document.writeln("①解特九肖：猴龙羊马虎<span style='background-color: #FFFF00\'>狗</span>牛蛇鸡<br>");
 document.writeln("②解特五肖：猴龙羊马虎<br>");
 document.writeln("③解特18码：10.34.02.14.23.35.12.24.16.28.08.20.29.41.13.25.09.21</b></font></p></td>");
 document.writeln("    </tr>     ");
