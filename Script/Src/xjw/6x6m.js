666d94c8a549567b7c9bc31e3f79f1eb6213b84b
Rokate <77772464+Rokate@users.noreply.github.com>
2025-09-15 23:20:36 +0800

    Update:  2025.09.15 23:20:36

--- Script/Src/xjw/6x6m.js
+++ Script/Src/xjw/6x6m.js
@@ -41,6 +41,14 @@ document.writeln("		");
 document.writeln("");
 
 
+document.writeln("");
+document.writeln("<tr>");
+document.writeln("			<td align=\'center\' height=42><b>");
+document.writeln("			<font style=\'font-size: 12pt\'>261期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(中00-中00-中00-中00-中00-中00)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开中00</font></b></td>");
+document.writeln("</tr>");
+document.writeln("");
+
+
 
 document.writeln("");
 document.writeln("<tr>");
@@ -52,20 +60,12 @@ document.writeln("");
 
 
 
-document.writeln("");
-document.writeln("<tr>");
-document.writeln("			<td align=\'center\' height=42><b>");
-document.writeln("			<font style=\'font-size: 12pt\'>259期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(中00-中00-中00-中00-中00-中00)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开中00</font></b></td>");
-document.writeln("</tr>");
-document.writeln("");
-
-
 
 
 document.writeln("");
 document.writeln("<tr>");
 document.writeln("			<td align=\'center\' height=42><b>");
-document.writeln("			<font style=\'font-size: 12pt\'>258期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(蛇25-马36-兔27-羊35-41牛-狗20)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
+document.writeln("			<font style=\'font-size: 12pt\'>259期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(鼠30-蛇25-马24-牛29-虎28-猴34)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
 document.writeln("</tr>");
 document.writeln("");
 
--- Script/Src/xjw/xj18m.js
+++ Script/Src/xjw/xj18m.js
@@ -29,7 +29,7 @@ document.writeln("</style>");
 document.writeln("");
 document.writeln("<body>");
 document.writeln("  ");
-document.writeln("<img src=\"https://amo.sanyiyuntong.com:4949/col/258/amzy.jpg\" alt=\"\" width=\"100%\">");
+document.writeln("<img src=\"https://amo.sanyiyuntong.com:4949/col/259/amzy.jpg\" alt=\"\" width=\"100%\">");
 document.writeln("");
 document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
 document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
@@ -48,6 +48,7 @@ document.writeln("");
 
 
 
+
 document.writeln("  <!--开始-->  ");
 document.writeln("    	<tr>");
 document.writeln("      <td height=38 >");
@@ -56,10 +57,29 @@ document.writeln("");
 document.writeln("		<font face=\'微软雅黑\'>");
 document.writeln("		<b>");
 document.writeln("");
-document.writeln("		<font color=\"#0000FF\">258期【玄机特码诗】猜透必中开（？00）</font><br>");
-document.writeln("①解特九肖：兔狗龙猴鼠羊鸡猪虎<br>");
-document.writeln("②解特五肖：兔狗龙猴鼠<br>");
-document.writeln("③解特18码：15.27.20.32.14.38.22.34.18.30.11.23.21.33.07.19.16.28</b></font></p></td>");
+document.writeln("		<font color=\"#0000FF\">259期【玄机特码诗】猜透必中开（？00）</font><br>");
+document.writeln("①解特九肖：虎猴牛鸡鼠猪马蛇龙<br>");
+document.writeln("②解特五肖：虎猴牛鸡鼠<br>");
+document.writeln("③解特18码：04.16.34.46.17.29.09.33.06.30.19.31.12.48.01.13.14.26</b></font></p></td>");
+document.writeln("    </tr>     ");
+document.writeln("     <!--结束-->");
+document.writeln("	");
+document.writeln("	");
+
+
+
+document.writeln("  <!--开始-->  ");
+document.writeln("    	<tr>");
+document.writeln("      <td height=38 >");
+document.writeln("		<p align=\'left\'>");
+document.writeln("");
+document.writeln("		<font face=\'微软雅黑\'>");
+document.writeln("		<b>");
+document.writeln("");
+document.writeln("		<font color=\"#0000FF\">258期【玄机特码诗】猜透必中开（龙38）</font><br>");
+document.writeln("①解特九肖：兔狗<span style='background-color: #FFFF00\'>龙</span>猴鼠羊鸡猪虎<br>");
+document.writeln("②解特五肖：兔狗<span style='background-color: #FFFF00\'>龙</span>猴鼠<br>");
+document.writeln("③解特18码：15.27.20.32.14.<span style='background-color: #FFFF00\'>38</span>.22.34.18.30.11.23.21.33.07.19.16.28</b></font></p></td>");
 document.writeln("    </tr>     ");
 document.writeln("     <!--结束-->");
 document.writeln("	");
