c5ddb1f2c5d4ef7fe0388a658294d20c74e08a15
Rokate <77772464+Rokate@users.noreply.github.com>
2025-10-01 02:20:40 +0800

    Update:  2025.10.01 02:20:40

--- Script/Src/xjw/3wzt.js
+++ Script/Src/xjw/3wzt.js
@@ -43,11 +43,24 @@ document.writeln("");
 document.writeln("");
 
 
+
 document.writeln("	<!----开始---->    ");
 document.writeln("<tr>");
 document.writeln("<td height=40 class=\'stylelxz\' align=\"center\">");
 document.writeln("<p align=\"center\"><font size=\"4\">");
-document.writeln("273期六尾</font><font color=\"#FF0000\" size=\"5\">（8-6-2-7-0-4）</font><font size=\"4\"><br>");
+document.writeln("274期六尾</font><font color=\"#FF0000\" size=\"5\">（5-4-6-3-8-1）</font><font size=\"4\"><br>");
+document.writeln("274期三尾</font><font color=\"#FF0000\" size=\"5\">（5-4-6）</font>");
+document.writeln("</td>");
+document.writeln("</tr>			");
+document.writeln("<!----结束----> ");
+document.writeln("");
+
+
+document.writeln("	<!----开始---->    ");
+document.writeln("<tr>");
+document.writeln("<td height=40 class=\'stylelxz\' align=\"center\">");
+document.writeln("<p align=\"center\"><font size=\"4\">");
+document.writeln("273期六尾</font><font color=\"#FF0000\" size=\"5\">（8-6-2-7-0-<span style='background-color: #FFFF00\'>4</span>）</font><font size=\"4\"><br>");
 document.writeln("273期三尾</font><font color=\"#FF0000\" size=\"5\">（8-6-2）</font>");
 document.writeln("</td>");
 document.writeln("</tr>			");
