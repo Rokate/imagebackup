a160ba79c94e8e2fd1e2312472a2265a1ee69078
Rokate <77772464+Rokate@users.noreply.github.com>
2025-10-02 00:20:38 +0800

    Update:  2025.10.02 00:20:38

--- Script/Src/xjw/6x6m.js
+++ Script/Src/xjw/6x6m.js
@@ -42,14 +42,13 @@ document.writeln("");
 
 
 
+
 document.writeln("");
 document.writeln("<tr>");
 document.writeln("			<td align=\'center\' height=42><b>");
-document.writeln("			<font style=\'font-size: 12pt\'>274期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(兔27-鼠30-龙26-虎28-牛17-蛇01)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
+document.writeln("			<font style=\'font-size: 12pt\'>275期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(猴34-狗44-鼠30-牛41-兔27-羊11)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
 document.writeln("</tr>");
 document.writeln("");
-
-
 
 
 
--- Script/Src/xjw/dx8m.js
+++ Script/Src/xjw/dx8m.js
@@ -47,9 +47,22 @@ document.writeln("	<!----开始---->    ");
 document.writeln("<tr>");
 document.writeln("<td height=40 class=\'stylelxz\' align=\"center\">");
 document.writeln("<p align=\"center\"><font size=\"4\">");
-document.writeln("274期玄机出版<font color=\"#0000FF\">【大小+8码】</font>开？00准</font><br>");
+document.writeln("275期玄机出版<font color=\"#0000FF\">【大小+8码】</font>开？00准</font><br>");
 document.writeln("<font color=\"#FF00FF\"><span style=\"font-size: 16pt\">");
-document.writeln("大数+01.04.08.12.14.17.21.22</span></font></td>");
+document.writeln("小数+32.34.38.39.42.43.46.49</span></font></td>");
+document.writeln("</tr>			");
+document.writeln("<!----结束----> ");
+document.writeln("	");
+
+
+
+document.writeln("	<!----开始---->    ");
+document.writeln("<tr>");
+document.writeln("<td height=40 class=\'stylelxz\' align=\"center\">");
+document.writeln("<p align=\"center\"><font size=\"4\">");
+document.writeln("274期玄机出版<font color=\"#0000FF\">【大小+8码】</font>开狗44准</font><br>");
+document.writeln("<font color=\"#FF00FF\"><span style=\"font-size: 16pt\">");
+document.writeln("<span style='background-color: #FFFF00\'>大数</span>+01.04.08.12.14.17.21.22</span></font></td>");
 document.writeln("</tr>			");
 document.writeln("<!----结束----> ");
 document.writeln("	");
