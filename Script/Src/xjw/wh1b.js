03de3585998893d433c1642398e6f70e9a2341a4
Rokate <77772464+Rokate@users.noreply.github.com>
2025-10-05 23:20:42 +0800

    Update:  2025.10.05 23:20:42

--- Script/Src/xjw/wh1b.js
+++ Script/Src/xjw/wh1b.js
@@ -43,16 +43,16 @@ document.writeln("");
 
 
 
+
 document.writeln("	<!--gavc-->										");
 document.writeln("	<tr>");
 document.writeln("		<td bgcolor=\"#FFFFFF\" height=\"38\" align=\"center\"> <b>");
 document.writeln("		");
-document.writeln("<span style=\"font-size: 16pt\">278期 网红一波<font color=\"#FF0000\">【红波】</font><BR>");
-document.writeln("网红双波<font color=\"#FF0000\">【红波蓝波】</font>开？00</span></td>");
+document.writeln("<span style=\"font-size: 16pt\">279期 网红一波<font color=\"#FF0000\">【红波】</font><BR>");
+document.writeln("网红双波<font color=\"#FF0000\">【红波绿波】</font>开？00</span></td>");
 document.writeln("	</tr>");
 document.writeln("	<!--结束-->	");
 document.writeln("");
-
 
 
 
@@ -64,7 +64,7 @@ document.writeln("	<tr>");
 document.writeln("		<td bgcolor=\"#FFFFFF\" height=\"38\" align=\"center\"> <b>");
 document.writeln("		");
 document.writeln("<span style=\"font-size: 16pt\">276期 网红一波<font color=\"#FF0000\">【<span style='background-color: #FFFF00\'>蓝波</span>】</font><BR>");
-document.writeln("网红双波<font color=\"#FF0000\">【<span style='background-color: #FFFF00\'>蓝波</span>绿波】</font>开？00</span></td>");
+document.writeln("网红双波<font color=\"#FF0000\">【<span style='background-color: #FFFF00\'>蓝波</span>绿波】</font>开虎28</span></td>");
 document.writeln("	</tr>");
 document.writeln("	<!--结束-->	");
 document.writeln("");
