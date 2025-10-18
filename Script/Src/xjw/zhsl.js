a824a9f862132e97e02daa6de14bdc18865af365
Rokate <77772464+Rokate@users.noreply.github.com>
2025-10-18 18:20:34 +0800

    Update:  2025.10.18 18:20:34

--- Script/Src/xjw/zhsl.js
+++ Script/Src/xjw/zhsl.js
@@ -1598,7 +1598,7 @@ document.writeln("		<!--开始五特-->");
 document.writeln("           <li>");
 document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff height=\'37\'><tbody><tr>");
 document.writeln("	<td width=100% height=37><p align=center><font face=\'楷体\'><b><font size=\'4\'>");
-document.writeln("291期杀6码:<font color=\'#ce0f67\' size=\'5\'>13.24.43.14.17</font>开？00√</font></b></font></table>");
+document.writeln("291期杀6码:<font color=\'#ce0f67\' size=\'5\'>13.24.43.14.17.35</font>开？00√</font></b></font></table>");
 document.writeln("</li>");
 document.writeln("		<!--开始五特-->");
 document.writeln("");
