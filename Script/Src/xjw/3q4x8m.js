f468d91e894e75e754eb989e393ed55670b282c9
Rokate <77772464+Rokate@users.noreply.github.com>
2025-10-25 09:20:39 +0800

    Update:  2025.10.25 09:20:39

--- Script/Src/xjw/3q4x8m.js
+++ Script/Src/xjw/3q4x8m.js
@@ -57,7 +57,7 @@ document.writeln("		<td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' widt
 document.writeln("		<b><font face=\'微软雅黑\'>299期</font></b></td>");
 document.writeln("		<td align=\'center\' bgcolor=\'#FFFFFF\' width=\'65%\' rowspan=\'3\'>");
 document.writeln("		<strong>");
-document.writeln("		<font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'><span style='background-color: #FFFF00\'><span style='background-color: #FFFF00\'>狗</span>猴龙马<br>");
+document.writeln("		<font face=\'微软雅黑\' size=\'5\' color=\'#FF0000\'><span style='background-color: #FFFF00\'>狗</span>猴龙马<br>");
 document.writeln("		</font>");
 document.writeln("		<font face=\'微软雅黑\' color=\'#FF0000\' size=\"4\">（20.<span style='background-color: #FFFF00\'>44</span>.10.46.02.26.12.48）</td>");
 document.writeln("		<td  height=\'11\' align=\'center\' bgcolor=\'#FFFFFF\' width=\'10%\'>");
