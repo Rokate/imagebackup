f5b586056661a8f086dbbe5e924ed5a72c94f9f7
Rokate <77772464+Rokate@users.noreply.github.com>
2025-12-19 23:20:39 +0800

    Update:  2025.12.19 23:20:39

--- Script/Src/xjw/6x6m.js
+++ Script/Src/xjw/6x6m.js
@@ -44,7 +44,17 @@ document.writeln("");
 document.writeln("");
 document.writeln("<tr>");
 document.writeln("			<td align=\'center\' height=42><b>");
-document.writeln("			<font style=\'font-size: 12pt\'>353期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(虎16-鸡21-蛇25-牛41-鼠30-狗32)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
+document.writeln("			<font style=\'font-size: 12pt\'>354期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(猴22-蛇25-龙02-兔15-狗20-鸡33)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
+document.writeln("</tr>");
+document.writeln("");
+
+
+
+
+document.writeln("");
+document.writeln("<tr>");
+document.writeln("			<td align=\'center\' height=42><b>");
+document.writeln("			<font style=\'font-size: 12pt\'>353期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(虎16-鸡21-<span style='background-color: #FFFF00\'>蛇</span>25-牛41-鼠30-狗32)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开蛇01</font></b></td>");
 document.writeln("</tr>");
 document.writeln("");
 
