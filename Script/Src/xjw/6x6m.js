6a50247e4c6f29cd380e76f1103bb1519983cf0a
Rokate <77772464+Rokate@users.noreply.github.com>
2026-04-20 23:24:37 +0800

    Update:  2026.04.20 23:24:37

--- Script/Src/xjw/6x6m.js
+++ Script/Src/xjw/6x6m.js
@@ -41,13 +41,13 @@ document.writeln("		");
 document.writeln("");
 
 
+
 document.writeln("");
 document.writeln("<tr>");
 document.writeln("			<td align=\'center\' height=42><b>");
-document.writeln("			<font style=\'font-size: 12pt\'>110期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(羊24-龙03-鼠09-鸡22-马37-兔16)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
+document.writeln("			<font style=\'font-size: 12pt\'>111期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(鸡22-虎29-蛇26-猪20-狗09-牛30)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
 document.writeln("</tr>");
 document.writeln("");
-
 
 
 
--- Script/Src/xjw/td6m.js
+++ Script/Src/xjw/td6m.js
@@ -32,18 +32,18 @@ document.writeln("");
 
 
 
-
-
 document.writeln("				<tr>");
 document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
 document.writeln("	");
 document.writeln("		<p>");
 document.writeln("	");
-document.writeln("		<span><b><font size=\'4\'>110期:</font><font color=\'#0000FF\' style=\"font-size: 14pt\">【天地六码】</font></b><font size=\'4\'><b><font color=\'#FF0000\'>开？00</font></b><font color=\'#008000\'><b>准</b><br>");
+document.writeln("		<span><b><font size=\'4\'>111期:</font><font color=\'#0000FF\' style=\"font-size: 14pt\">【天地六码】</font></b><font size=\'4\'><b><font color=\'#FF0000\'>开？00</font></b><font color=\'#008000\'><b>准</b><br>");
 document.writeln("		</font></font>");
 document.writeln("		<b>");
-document.writeln("		<font style=\"font-size: 16pt\">地肖【蛇羊鸡】<br>26.38.12.24.34.46</font></b></span></tr>	");
+document.writeln("		<font style=\"font-size: 16pt\">天肖【龙马猴】<br>15.27.25.37.23.35</font></b></span></tr>	");
 document.writeln("");
+
+
 
 
 document.writeln("				<tr>");
