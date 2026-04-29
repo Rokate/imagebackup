0caa0614a7a728adc4bcb2300b2adc28c4420e5e
Rokate <77772464+Rokate@users.noreply.github.com>
2026-04-30 00:23:50 +0800

    Update:  2026.04.30 00:23:50

--- Script/Src/xjw/6x6m.js
+++ Script/Src/xjw/6x6m.js
@@ -43,11 +43,10 @@ document.writeln("");
 
 
 
-
 document.writeln("");
 document.writeln("<tr>");
 document.writeln("			<td align=\'center\' height=42><b>");
-document.writeln("			<font style=\'font-size: 12pt\'>119期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(猪32-羊24-虎29-马37-龙39-兔28)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
+document.writeln("			<font style=\'font-size: 12pt\'>120期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(羊24-马25-狗33-鼠31-蛇02-猪08)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
 document.writeln("</tr>");
 document.writeln("");
 
