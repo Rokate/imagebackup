d24d4ebcee77251445bd5cdfeb9e5408656c5177
Rokate <77772464+Rokate@users.noreply.github.com>
2026-06-07 00:20:35 +0800

    Update:  2026.06.07 00:20:35

--- Script/Src/xjw/td6m.js
+++ Script/Src/xjw/td6m.js
@@ -30,6 +30,17 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 
+document.writeln("				<tr>");
+document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
+document.writeln("	");
+document.writeln("		<p>");
+document.writeln("	");
+document.writeln("		<span><b><font size=\'4\'>158期:</font><font color=\'#0000FF\' style=\"font-size: 14pt\">【天地六码】</font></b><font size=\'4\'><b><font color=\'#FF0000\'>开？00</font></b><font color=\'#008000\'><b>准</b><br>");
+document.writeln("		</font></font>");
+document.writeln("		<b>");
+document.writeln("		<font style=\"font-size: 16pt\">天肖【猪马猴】<br>20.32.25.49.23.35</font></b></span></tr>	");
+document.writeln("");
+
 
 
 
@@ -38,10 +49,10 @@ document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
 document.writeln("	");
 document.writeln("		<p>");
 document.writeln("	");
-document.writeln("		<span><b><font size=\'4\'>157期:</font><font color=\'#0000FF\' style=\"font-size: 14pt\">【天地六码】</font></b><font size=\'4\'><b><font color=\'#FF0000\'>开？00</font></b><font color=\'#008000\'><b>准</b><br>");
+document.writeln("		<span><b><font size=\'4\'>157期:</font><font color=\'#0000FF\' style=\"font-size: 14pt\">【天地六码】</font></b><font size=\'4\'><b><font color=\'#FF0000\'>开兔40</font></b><font color=\'#008000\'><b>准</b><br>");
 document.writeln("		</font></font>");
 document.writeln("		<b>");
-document.writeln("		<font style=\"font-size: 16pt\">天肖【猪牛龙】<br>20.32.18.30.15.27</font></b></span></tr>	");
+document.writeln("		<font style=\"font-size: 16pt\"><span style='background-color: #FFFF00\'>天肖</span>【猪牛龙】<br>20.32.18.30.15.27</font></b></span></tr>	");
 document.writeln("");
 
 
