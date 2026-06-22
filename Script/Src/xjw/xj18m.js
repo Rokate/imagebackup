6667497e2d24e33dc60f6b0662dacdbed06961f9
Rokate <77772464+Rokate@users.noreply.github.com>
2026-06-22 23:20:43 +0800

    Update:  2026.06.22 23:20:43

--- Script/Src/xjw/xj18m.js
+++ Script/Src/xjw/xj18m.js
@@ -29,7 +29,7 @@ document.writeln("</style>");
 document.writeln("");
 document.writeln("<body>");
 document.writeln("  ");
-document.writeln("<img src=\"https://amo.jlidesign.com:4949/col/173/amzy.jpg\" alt=\"\" width=\"100%\">");
+document.writeln("<img src=\"https://amo.jlidesign.com:4949/col/174/amzy.jpg\" alt=\"\" width=\"100%\">");
 document.writeln("");
 document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
 document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
@@ -54,10 +54,30 @@ document.writeln("");
 document.writeln("		<font face=\'微软雅黑\'>");
 document.writeln("		<b>");
 document.writeln("");
-document.writeln("		<font color=\"#0000FF\">173期【玄机特码诗】猜透必中开（？00）</font><br>");
-document.writeln("①解特九肖：猴牛蛇鸡虎马龙羊兔<br>");
-document.writeln("②解特五肖：猴牛蛇鸡虎<br>");
-document.writeln("③解特18码：11.23.30.42.26.38.22.34.29.41.13.25.03.15.12.24.28.40</b></font></p></td>");
+document.writeln("		<font color=\"#0000FF\">174期【玄机特码诗】猜透必中开（？00）</font><br>");
+document.writeln("①解特九肖：兔马鼠鸡猴牛虎龙蛇 <br>");
+document.writeln("②解特五肖：兔马鼠鸡猴<br>");
+document.writeln("③解特18码：16.28.25.37.19.31.22.34.35.47.18.30.05.17.27.39.02.14</b></font></p></td>");
+document.writeln("    </tr>     ");
+document.writeln("     <!--结束-->");
+document.writeln("	");
+document.writeln("	");
+
+
+
+
+document.writeln("  <!--开始-->  ");
+document.writeln("    	<tr>");
+document.writeln("      <td height=38 >");
+document.writeln("		<p align=\'left\'>");
+document.writeln("");
+document.writeln("		<font face=\'微软雅黑\'>");
+document.writeln("		<b>");
+document.writeln("");
+document.writeln("		<font color=\"#0000FF\">173期【玄机特码诗】猜透必中开（蛇26）</font><br>");
+document.writeln("①解特九肖：猴牛<span style='background-color: #FFFF00\'>蛇</span>鸡虎马龙羊兔<br>");
+document.writeln("②解特五肖：猴牛<span style='background-color: #FFFF00\'>蛇</span>鸡虎<br>");
+document.writeln("③解特18码：11.23.30.42.<span style='background-color: #FFFF00\'>26</span>.38.22.34.29.41.13.25.03.15.12.24.28.40</b></font></p></td>");
 document.writeln("    </tr>     ");
 document.writeln("     <!--结束-->");
 document.writeln("	");
