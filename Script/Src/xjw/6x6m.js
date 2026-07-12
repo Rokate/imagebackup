9658f1c51386a9774bb5282cc708a282f041c77c
Rokate <77772464+Rokate@users.noreply.github.com>
2026-07-13 00:20:40 +0800

    Update:  2026.07.13 00:20:40

--- Script/Src/xjw/6x6m.js
+++ Script/Src/xjw/6x6m.js
@@ -45,11 +45,9 @@ document.writeln("");
 document.writeln("");
 document.writeln("<tr>");
 document.writeln("			<td align=\'center\' height=42><b>");
-document.writeln("			<font style=\'font-size: 12pt\'>193期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(兔28-猪32-猴35-牛30-蛇14-鼠31)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
+document.writeln("			<font style=\'font-size: 12pt\'>194期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(蛇26-马37-兔28-羊36-牛42-狗21)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
 document.writeln("</tr>");
 document.writeln("");
-
-
 
 
 
