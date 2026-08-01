271626024cb0551463e6558c42da81e1246b59da
Rokate <77772464+Rokate@users.noreply.github.com>
2026-08-01 23:20:59 +0800

    Update:  2026.08.01 23:20:59

--- Script/Src/xjw/xj18m.js
+++ Script/Src/xjw/xj18m.js
@@ -29,7 +29,7 @@ document.writeln("</style>");
 document.writeln("");
 document.writeln("<body>");
 document.writeln("  ");
-document.writeln("<img src=\"https://amo.jlidesign.com:4949/col/213/amzy.jpg\" alt=\"\" width=\"100%\">");
+document.writeln("<img src=\"https://amo.jlidesign.com:4949/col/214/amzy.jpg\" alt=\"\" width=\"100%\">");
 document.writeln("");
 document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
 document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
@@ -47,6 +47,7 @@ document.writeln("");
 
 
 
+
 document.writeln("  <!--开始-->  ");
 document.writeln("    	<tr>");
 document.writeln("      <td height=38 >");
@@ -55,10 +56,29 @@ document.writeln("");
 document.writeln("		<font face=\'微软雅黑\'>");
 document.writeln("		<b>");
 document.writeln("");
-document.writeln("		<font color=\"#0000FF\">213期【玄机特码诗】猜透必中开（？00）</font><br>");
-document.writeln("①解特九肖：兔狗龙猴鼠羊鸡猪虎<br>");
-document.writeln("②解特五肖：兔狗龙猴鼠<br>");
-document.writeln("③解特18码：16.28.21.33.15.39.23.35.19.31.12.24.22.34.08.20.17.29</b></font></p></td>");
+document.writeln("		<font color=\"#0000FF\">214期【玄机特码诗】猜透必中开（？00）</font><br>");
+document.writeln("①解特九肖：兔马鼠鸡猴牛虎龙蛇<br>");
+document.writeln("②解特五肖：兔马鼠鸡猴<br>");
+document.writeln("③解特18码：16.28.25.37.19.31.22.34.35.47.18.30.05.17.27.39.02.14</b></font></p></td>");
+document.writeln("    </tr>     ");
+document.writeln("     <!--结束-->");
+document.writeln("	");
+document.writeln("	");
+
+
+
+document.writeln("  <!--开始-->  ");
+document.writeln("    	<tr>");
+document.writeln("      <td height=38 >");
+document.writeln("		<p align=\'left\'>");
+document.writeln("");
+document.writeln("		<font face=\'微软雅黑\'>");
+document.writeln("		<b>");
+document.writeln("");
+document.writeln("		<font color=\"#0000FF\">213期【玄机特码诗】猜透必中开（猴35）</font><br>");
+document.writeln("①解特九肖：兔狗龙<span style='background-color: #FFFF00\'>猴</span>鼠羊鸡猪虎<br>");
+document.writeln("②解特五肖：兔狗龙<span style='background-color: #FFFF00\'>猴</span>鼠<br>");
+document.writeln("③解特18码：16.28.21.33.15.39.23.<span style='background-color: #FFFF00\'>35</span>.19.31.12.24.22.34.08.20.17.29</b></font></p></td>");
 document.writeln("    </tr>     ");
 document.writeln("     <!--结束-->");
 document.writeln("	");
