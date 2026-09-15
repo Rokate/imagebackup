62f22f964d477784b059a1b44e514197bd93c6eb
Rokate <77772464+Rokate@users.noreply.github.com>
2026-09-16 01:20:40 +0800

    Update:  2026.09.16 01:20:40

--- Script/Src/xjw/3wzt.js
+++ Script/Src/xjw/3wzt.js
@@ -42,12 +42,25 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 
+
 document.writeln("	<!----开始---->    ");
 document.writeln("<tr>");
 document.writeln("<td height=40 class=\'stylelxz\' align=\"center\">");
 document.writeln("<p align=\"center\"><font size=\"4\">");
-document.writeln("258期六尾</font><font color=\"#FF0000\" size=\"5\">（7-6-5-8-1-0）</font><font size=\"4\"><br>");
-document.writeln("258期三尾</font><font color=\"#FF0000\" size=\"5\">（7-6-5）</font>");
+document.writeln("259期六尾</font><font color=\"#FF0000\" size=\"5\">（4-0-2-6-9-5）</font><font size=\"4\"><br>");
+document.writeln("259期三尾</font><font color=\"#FF0000\" size=\"5\">（4-0-2）</font>");
+document.writeln("</td>");
+document.writeln("</tr>			");
+document.writeln("<!----结束----> ");
+document.writeln("");
+
+
+document.writeln("	<!----开始---->    ");
+document.writeln("<tr>");
+document.writeln("<td height=40 class=\'stylelxz\' align=\"center\">");
+document.writeln("<p align=\"center\"><font size=\"4\">");
+document.writeln("258期六尾</font><font color=\"#FF0000\" size=\"5\">（7-<span style='background-color: #FFFF00\'>6</span>-5-8-1-0）</font><font size=\"4\"><br>");
+document.writeln("258期三尾</font><font color=\"#FF0000\" size=\"5\">（7-<span style='background-color: #FFFF00\'>6</span>-5）</font>");
 document.writeln("</td>");
 document.writeln("</tr>			");
 document.writeln("<!----结束----> ");
--- Script/Src/xjw/6x6m.js
+++ Script/Src/xjw/6x6m.js
@@ -41,13 +41,14 @@ document.writeln("		");
 document.writeln("");
 
 
-
 document.writeln("");
 document.writeln("<tr>");
 document.writeln("			<td align=\'center\' height=42><b>");
-document.writeln("			<font style=\'font-size: 12pt\'>258期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(马25-猪20-猴23-兔28-蛇02-牛30)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
+document.writeln("			<font style=\'font-size: 12pt\'>259期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(猴23-猪08-虎05-龙15-兔16-狗21)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
 document.writeln("</tr>");
 document.writeln("");
+
+
 
 
 
