c586da100cc043792b7ef6ce3c25694ab658c6db
Rokate <77772464+Rokate@users.noreply.github.com>
2026-09-18 00:20:52 +0800

    Update:  2026.09.18 00:20:52

--- Script/Src/xjw/6x6m.js
+++ Script/Src/xjw/6x6m.js
@@ -41,34 +41,12 @@ document.writeln("		");
 document.writeln("");
 
 
-
 document.writeln("");
 document.writeln("<tr>");
 document.writeln("			<td align=\'center\' height=42><b>");
-document.writeln("			<font style=\'font-size: 12pt\'>260期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(虎29-鸡22-马25-蛇02-鸡46-羊36)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
+document.writeln("			<font style=\'font-size: 12pt\'>261期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(虎05-兔28-羊36-蛇38-龙03-羊12)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开？00</font></b></td>");
 document.writeln("</tr>");
 document.writeln("");
-
-
-
-
-document.writeln("");
-document.writeln("<tr>");
-document.writeln("			<td align=\'center\' height=42><b>");
-document.writeln("			<font style=\'font-size: 12pt\'>257期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(<span style='background-color: #FFFF00\'>鼠</span>19-羊24-猪32-狗21-兔28-鸡34)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开鼠07</font></b></td>");
-document.writeln("</tr>");
-document.writeln("");
-
-
-
-
-document.writeln("");
-document.writeln("<tr>");
-document.writeln("			<td align=\'center\' height=42><b>");
-document.writeln("			<font style=\'font-size: 12pt\'>254期</font><font color=\'#0000FF\' style=\'font-size: 12pt\'>(兔28-鼠31-龙27-虎29-牛18-<span style='background-color: #FFFF00\'>蛇02</span>)</font><font color=\'#FF0000\' style=\'font-size: 12pt\'>开蛇02</font></b></td>");
-document.writeln("</tr>");
-document.writeln("");
-
 
 
 
--- Script/Src/xjw/td6m.js
+++ Script/Src/xjw/td6m.js
@@ -38,11 +38,13 @@ document.writeln("		<td class=\'center f11 black l150\' align=\'center\'>");
 document.writeln("	");
 document.writeln("		<p>");
 document.writeln("	");
-document.writeln("		<span><b><font size=\'4\'>260期:</font><font color=\'#0000FF\' style=\"font-size: 14pt\">【天地六码】</font></b><font size=\'4\'><b><font color=\'#FF0000\'>开？00</font></b><font color=\'#008000\'><b>准</b><br>");
+document.writeln("		<span><b><font size=\'4\'>261期:</font><font color=\'#0000FF\' style=\"font-size: 14pt\">【天地六码】</font></b><font size=\'4\'><b><font color=\'#FF0000\'>开？00</font></b><font color=\'#008000\'><b>准</b><br>");
 document.writeln("		</font></font>");
 document.writeln("		<b>");
-document.writeln("		<font style=\"font-size: 16pt\">地肖【蛇羊鸡】<br>26.38.24.36.34.46</font></b></span></tr>	");
+document.writeln("		<font style=\"font-size: 16pt\">天肖【牛兔龙】<br>06.18.28.40.15.27</font></b></span></tr>	");
 document.writeln("");
+
+
 
 
 
