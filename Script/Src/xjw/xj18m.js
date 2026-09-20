95bbf40d588b9801f8dad8744eee150c34025e4e
Rokate <77772464+Rokate@users.noreply.github.com>
2026-09-20 23:20:37 +0800

    Update:  2026.09.20 23:20:37

--- Script/Src/xjw/xj18m.js
+++ Script/Src/xjw/xj18m.js
@@ -29,7 +29,7 @@ document.writeln("</style>");
 document.writeln("");
 document.writeln("<body>");
 document.writeln("  ");
-document.writeln("<img src=\"https://amo.jlidesign.com:4949/col/263/amzy.jpg\" alt=\"\" width=\"100%\">");
+document.writeln("<img src=\"https://amo.jlidesign.com:4949/col/264/amzy.jpg\" alt=\"\" width=\"100%\">");
 document.writeln("");
 document.writeln("  <table border=1 width=100% bgcolor=#ffffff><tbody>");
 document.writeln("<td style=\'border:10px double #8bafaf\' bgcolor=#8bafaf height=50><p align=center>");
@@ -47,6 +47,9 @@ document.writeln("");
 
 
 
+
+
+
 document.writeln("  <!--开始-->  ");
 document.writeln("    	<tr>");
 document.writeln("      <td height=38 >");
@@ -55,14 +58,16 @@ document.writeln("");
 document.writeln("		<font face=\'微软雅黑\'>");
 document.writeln("		<b>");
 document.writeln("");
-document.writeln("		<font color=\"#0000FF\">263期【玄机特码诗】猜透必中开（？00）</font><br>");
-document.writeln("①解特九肖：猴牛蛇鸡虎马龙羊兔<br>");
-document.writeln("②解特五肖：猴牛蛇鸡虎<br>");
-document.writeln("③解特18码：23.35.06.18.02.14.22.34.05.17.13.25.03.15.12.48.28.40</b></font></p></td>");
+document.writeln("		<font color=\"#0000FF\">264期【玄机特码诗】猜透必中开（？00）</font><br>");
+document.writeln("①解特九肖：羊牛猴龙狗兔马鸡虎 <br>");
+document.writeln("②解特五肖：羊牛猴龙狗<br>");
+document.writeln("③解特18码：24.48.18.30.23.35.03.39.33.45.28.40.13.25.22.46.04.16</b></font></p></td>");
 document.writeln("    </tr>     ");
 document.writeln("     <!--结束-->");
 document.writeln("	");
 document.writeln("	");
+
+
 
 
 
